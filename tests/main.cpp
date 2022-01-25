4c52a98a7f6dec1fe522a460cc9e8fc10ec28922
Matthew Marshall <matthew.marshall090396@gmail.com>
2022-01-25 21:46:41 +0000

    Add a function for funsies.

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,5 +1,13 @@
+#include "stdafx.h"
+
 #include <iostream>
 
-int main() {
+ui32 add(ui32 a, ui16 b) {
+    return a + static_cast<ui32>(b);
+}
+
+ui32 main() {
+    std::cout << "1 + 5 = " << add(1, 5) << std::endl;
+
     std::cout << "Hello, world!" << std::endl;
 }
