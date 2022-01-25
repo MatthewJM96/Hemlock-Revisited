20e692da03f64a093a7da67b5f326d975a129604
Matthew Marshall <matthew.marshall090396@gmail.com>
2022-01-25 21:40:42 +0000

    Introduce precompiled header and some type relabelling.

---
+++ include/stdafx.h
@@ -0,0 +1,34 @@
+#pragma once
+
+/******************\
+ * Std Containers *
+\******************/
+
+#include <array>
+#include <map>
+#include <queue>
+#include <set>
+#include <tuple>
+#include <unordered_map>
+#include <vector>
+
+/***************\
+ * Std Strings *
+\***************/
+
+#include <cstring>
+#include <string>
+
+/***************\
+ * Std Streams *
+\***************/
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+/*********************************\
+ * Consistent Scalar Type Labels *
+\*********************************/
+
+#include "types.h"
