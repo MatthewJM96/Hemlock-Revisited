bca0708bca2c8f0451c6a17ff0224fa37a3a27cd
Matthew Marshall <matthew.marshall090396@gmail.com>
2022-01-25 21:54:04 +0000

    Create CMakeLists.txt and fix first bug lol.

--- tests/main.cpp
+++ tests/main.cpp
@@ -6,7 +6,7 @@ ui32 add(ui32 a, ui16 b) {
     return a + static_cast<ui32>(b);
 }
 
-ui32 main() {
+i32 main() {
     std::cout << "1 + 5 = " << add(1, 5) << std::endl;
 
     std::cout << "Hello, world!" << std::endl;
