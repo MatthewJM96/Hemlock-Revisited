a16126019973609460566406f2e82b426a8069cd
Matthew Marshall <matthew.marshall090396@gmail.com>
2022-01-26 22:42:52 +0000

    Make types a module.

---
+++ src/types.cpp
@@ -0,0 +1,17 @@
+export module hemlock.types
+
+import <cstdint>;
+
+export {
+    typedef int8_t     i8; ///< 8-bit signed integer
+    typedef int16_t   i16; ///< 16-bit signed integer
+    typedef int32_t   i32; ///< 32-bit signed integer
+    typedef int64_t   i64; ///< 64-bit signed integer
+    typedef uint8_t   ui8; ///< 8-bit unsigned integer
+    typedef uint16_t ui16; ///< 16-bit unsigned integer
+    typedef uint32_t ui32; ///< 32-bit unsigned integer
+    typedef uint64_t ui64; ///< 64-bit unsigned integer
+
+    typedef float  f32; ///< 32-bit floating point value (single)
+    typedef double f64; ///< 64-bit floating point value (double)
+}
