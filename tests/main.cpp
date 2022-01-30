7e77335d52bb43585069f884043c92ad055e4b8a
Matthew Marshall <matthew.marshall090396@gmail.com>
2022-01-30 04:22:51 +0000

    Create a super dumb starting app.

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,8 +1,36 @@
 #include "stdafx.h"
 
-#include "graphics/window.h"
+#include "app/app.h"
+#include "app/screen.h"
 
 void add(hemlock::Sender, ui32 a, ui32 b) { std::cout << a << " + " << b << " = " << a + b << std::endl; }
+
+class MyScreen : public happ::IScreen {
+public:
+    virtual ~MyScreen() { /* Empty */ };
+
+    virtual void update(TimeData time [[maybe_unused]]) override { /* Empty. */ }
+    virtual void draw(TimeData time [[maybe_unused]])   override { /* Empty. */ }
+
+    virtual void init(std::string name) override {
+        happ::IScreen::init(name);
+
+        m_state = happ::ScreenState::RUNNING;
+    }
+};
+
+class MyApp : public happ::BasicApp {
+public:
+    virtual ~MyApp() { /* Empty */ };
+protected:      
+    virtual void prepare_screens() override {
+        happ::IScreen* my_screen = new MyScreen();
+
+        m_screens.insert({ "my_screen", my_screen });
+
+        m_current_screen = my_screen;
+    }
+};
 
 i32 main() {
     hemlock::Event<ui32, ui32> on_calc;
@@ -16,12 +44,10 @@ i32 main() {
 
     on_calc(1, 4);
 
-    hg::Window window({
-        .dimensions={1024, 800}
-    });
-    window.init();
+    MyApp app;
+    app.init();
 
     std::cout << "Hello, world!" << std::endl;
 
-    while (true) {}
+    app.run();
 }
