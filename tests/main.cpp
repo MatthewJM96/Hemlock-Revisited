f804ff396046184223b5c59cce85ac73170f495a
Matthew Marshall <matthew.marshall090396@gmail.com>
2022-02-05 01:47:58 +0000

    Add simple test of thread pool.

--- tests/main.cpp
+++ tests/main.cpp
@@ -4,6 +4,24 @@
 #include "app/screen.h"
 #include "graphics/window_manager.h"
 
+struct ThreadContext {
+    bool stop = false;
+    std::string message = "";
+};
+
+class MyPrinterTask : public hemlock::IThreadTask<ThreadContext> {
+public:
+    virtual void execute(hemlock::Thread<ThreadContext>::State* state, hemlock::TaskQueue<ThreadContext>* task_queue) override {
+        state->context.message += "hello ";
+
+        std::cout << state->context.message << std::endl;
+
+        sleep(1);
+
+        task_queue->enqueue(state->producer_token, new MyPrinterTask());
+    }
+};
+
 void add(hemlock::Sender, ui32 a, ui32 b) { std::cout << a << " + " << b << " = " << a + b << std::endl; }
 
 class MyScreen : public happ::ScreenBase {
@@ -39,6 +57,23 @@ protected:
 };
 
 i32 main() {
+    hemlock::ThreadPool<ThreadContext> pool;
+    pool.init(3);
+
+    MyPrinterTask my_tasks[3] = {
+        MyPrinterTask(),
+        MyPrinterTask(),
+        MyPrinterTask()
+    };
+
+    MyPrinterTask* my_task_ptrs[3] = {
+        &my_tasks[0],
+        &my_tasks[1],
+        &my_tasks[2]
+    };
+
+    pool.add_tasks((hemlock::IThreadTask<ThreadContext>**)my_task_ptrs, 3);
+
     hemlock::Event<ui32, ui32> on_calc;
 
     auto listener = on_calc.add_functor(add);
@@ -57,6 +92,11 @@ i32 main() {
 
     MyApp app;
     app.init();
+
+    auto quit_handler = hemlock::Delegate<void(hemlock::Sender)>([&pool](hemlock::Sender) {
+        pool.dispose();
+    });
+    app.on_quit += &quit_handler;
 
     auto [ my_second_window, err ] = app.window_manager()->add_window();
 
