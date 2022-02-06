823e7800295157728d65dcca6418fa7d13245f3a
Matthew Marshall <matthew.marshall090396@gmail.com>
2022-02-06 03:03:36 +0000

    Implement a first pass GLSL program class.

---
+++ include/graphics/glsl_program.h
@@ -0,0 +1,150 @@
+#ifndef __hemlock_graphics_glsl_program_h
+#define __hemlock_graphics_glsl_program_h
+
+namespace hemlock {
+    namespace graphics {
+        /**
+         * @brief Enumerates the types of shader.
+         */
+        enum class ShaderType {
+            FRAGMENT = GL_FRAGMENT_SHADER,
+            VERTEX   = GL_VERTEX_SHADER
+        };
+
+        /**
+         * @brief Information needed to compile a shader.
+         */
+        struct ShaderInfo {
+            ShaderType  type;
+            std::string filepath;
+        };
+
+        using ShaderAttribute    = std::pair<std::string, GLuint>;
+        using ShaderAttributes   = std::vector<ShaderAttribute>;
+        using ShaderAttributeMap = std::map<std::string, GLuint>;
+
+        enum class ShaderCreationResult {
+            SUCCESS       =  0,
+            NON_EDITABLE  = -1,
+            VERTEX_EXISTS = -2,
+            FRAG_EXISTS   = -3,
+            INVALID_STAGE = -4,
+            CREATE_FAIL   = -5,
+            READ_FAIL     = -6,
+            COMPILE_FAIL  = -7
+        };
+
+        struct ShaderCreationResults {
+            ShaderCreationResult vertex, fragment;
+        };
+
+        enum class ShaderLinkResult {
+            SUCCESS        =  0,
+            NON_EDITABLE   = -1,
+            VERTEX_MISSING = -2,
+            FRAG_MISSING   = -3,
+            LINK_FAIL      = -4
+        };
+
+        class ShaderCache : public Cache<std::string, std::unordered_map<std::string, std::string>> {};
+
+        class GLSLProgram {
+        public:
+            GLSLProgram();
+            ~GLSLProgram() { /* Empty */ }
+
+            /**
+             * @brief Initialises a shader program.
+             *
+             * @param shader_cache The cache through which to obtain
+             * shader code.
+             */
+            void init(ShaderCache* shader_cache);
+            /**
+             * @brief Disposes of a shader program.
+             */
+            void dispose();
+
+            /**
+             * @brief Returns the ID of the shader program.
+             *
+             * @return The ID of the shader program.
+             */
+            GLuint id() const { return m_id; }
+
+            bool initialised() const { return m_id != 0;                    }
+            bool linked()      const { return m_is_linked;                  }
+            bool editable()    const { return !linked() && initialised();   }
+            bool in_use()      const { return m_id == GLSLProgram::current; }
+
+            /**
+             * @brief Adds a shader to the program.
+             *
+             * @param shader The information regarding the shader to be added.
+             *
+             * @return True if the shader is successfully added, false otherwise.
+             */
+            ShaderCreationResult add_shader(const ShaderInfo& shader);
+            /**
+             * @brief Adds both a vertex and a fragment shader to the program.
+             *
+             * @param vertex_path The filepath of the vertex shader to be added.
+             * @param fragment_path The filepath of the fragment shader to be added.
+             *
+             * @return True if the shaders are successfully added, false otherwise.
+             */
+            ShaderCreationResults add_shaders(const std::string& vertex_path, const std::string& fragment_path);
+
+            /**
+             * @brief Links the shaders to the shader program.
+             *
+             * @return True if the shaders are successfully linked, false otherwise.
+             */
+            ShaderLinkResult link();
+
+            /**
+             * @brief Sets an attribute with the given name to the given index.
+             */
+            bool set_attribute(const std::string& name, GLuint index);
+            /**
+             * @brief Sets an attribute with the given name to the given index.
+             */
+            bool set_attribute(const ShaderAttribute& attribute);
+            /**
+             * @brief Sets all attributes provided.
+             */
+            bool set_attributes(const ShaderAttributes& attributes);
+
+            // TODO(Matthew): If we add shaders from source instead of filepath, could parse location of uniforms that explicitly set it using "layout(location = X)".
+            GLuint attribute_location(const std::string& name) const { return m_attributes.at(name); }
+            GLuint uniform_location(const std::string& name)   const;
+
+            void enable_vertex_attrib_arrays()  const;
+            void disable_vertex_attrib_arrays() const;
+            bool enable_vertex_attrib_array(const std::string& name)  const;
+            bool disable_vertex_attrib_array(const std::string& name) const;
+
+            /**
+             * @brief Uses this shader program.
+             */
+            void use();
+            /**
+             * @brief Unuses the currently used shader program.
+             */
+            static void unuse();
+
+            static GLuint current;
+        protected:
+            GLuint m_id;
+            GLuint m_vertex_id, m_frag_id;
+            bool   m_is_linked;
+
+            ShaderAttributeMap m_attributes;
+
+            ShaderCache* m_shader_cache;
+        };
+    }
+}
+namespace hg  = hemlock::graphics;
+
+#endif // __hemlock_graphics_glsl_program_h
---
+++ src/graphics/glsl_program.cpp
@@ -0,0 +1,234 @@
+#include "stdafx.h"
+
+#include "graphics/glsl_program.h"
+
+GLuint hg::GLSLProgram::current = 0;
+
+hg::GLSLProgram::GLSLProgram() :
+    m_id(0),
+    m_vertex_id(0), m_frag_id(0),
+    m_is_linked(false)
+{
+    /* Empty */
+}
+
+void hg::GLSLProgram::init(ShaderCache* shader_cache) {
+    if (initialised()) return;
+    m_id = glCreateProgram();
+
+    m_shader_cache = shader_cache;
+}
+
+void hg::GLSLProgram::dispose() {
+    // Clear the vertex shader if it exists.
+    if (m_vertex_id != 0) {
+        glDeleteShader(m_vertex_id);
+        m_vertex_id = 0;
+    }
+
+    // Clear the fragment shader if it exists.
+    if (m_frag_id != 0) {
+        glDeleteShader(m_frag_id);
+        m_frag_id = 0;
+    }
+
+    // Clear the shader program if it exists.
+    if (m_id != 0) {
+        glDeleteProgram(m_id);
+        m_id = 0;
+        m_is_linked = false;
+    }
+
+    // Clear the attribute map.
+    ShaderAttributeMap().swap(m_attributes);
+}
+
+hg::ShaderCreationResult hg::GLSLProgram::add_shader(const ShaderInfo& shader) {
+    // If the program is in an uneditable state, fail.
+    if (!editable()) {
+        return ShaderCreationResult::NON_EDITABLE;
+    }
+
+    // Ensure we are targetting a valid shader type, that is not yet built.
+    switch (shader.type) {
+        case ShaderType::VERTEX:
+            if (m_vertex_id != 0) return ShaderCreationResult::VERTEX_EXISTS;
+            break;
+        case ShaderType::FRAGMENT:
+            if (m_frag_id != 0) return ShaderCreationResult::FRAG_EXISTS;
+            break;
+        default:
+            return ShaderCreationResult::INVALID_STAGE;
+    }
+
+    // Create the shader, ready for compilation.
+    GLuint shader_id = glCreateShader(static_cast<GLenum>(shader.type));
+    if (shader_id == 0) return ShaderCreationResult::CREATE_FAIL;
+
+    // Read in the shader code.
+    std::string* shader_code = m_shader_cache->fetch(shader.filepath);
+    if (!shader_code) {
+        return ShaderCreationResult::READ_FAIL;
+    }
+    char* buffer = (*shader_code).data();
+
+    // Compile our shader code.
+    glShaderSource(shader_id, 1, &buffer, nullptr);
+    glCompileShader(shader_id);
+
+    // Check if we succeeded in compilation.
+    GLint status = 0;
+    glGetShaderiv(shader_id, GL_COMPILE_STATUS, &status);
+    if (status == GL_FALSE) {
+        GLint max_length = 0;
+        glGetShaderiv(shader_id, GL_INFO_LOG_LENGTH, &max_length);
+
+        char* log = new char[max_length + 1];
+        glGetShaderInfoLog(shader_id, max_length, nullptr, log);
+        log[max_length] = '\0';
+
+        // TODO(Matthew): Send out log somehow (event?).
+
+        glDeleteShader(shader_id);
+        return ShaderCreationResult::COMPILE_FAIL;
+    }
+
+    // Set the appropriate shader ID.
+    switch(shader.type) {
+        case ShaderType::VERTEX:
+            m_vertex_id = shader_id;
+            break;
+        case ShaderType::FRAGMENT:
+            m_frag_id   = shader_id;
+            break;
+    }
+
+    return ShaderCreationResult::SUCCESS;
+}
+
+hg::ShaderCreationResults hg::GLSLProgram::add_shaders(const std::string& vertex_path, const std::string& fragment_path) {
+    return ShaderCreationResults{
+        add_shader(ShaderInfo{ ShaderType::VERTEX,   vertex_path   }),
+        add_shader(ShaderInfo{ ShaderType::FRAGMENT, fragment_path })
+    };
+}
+
+hg::ShaderLinkResult hg::GLSLProgram::link() {
+    // If the program is in an uneditable state, fail.
+    if (!editable()) return ShaderLinkResult::NON_EDITABLE;
+
+    // If we are missing either shader, fail.
+    if (!m_vertex_id) return ShaderLinkResult::VERTEX_MISSING;
+    if (!m_frag_id)   return ShaderLinkResult::FRAG_MISSING;
+
+    // Attach our shaders, link program and then detach shaders.
+    glAttachShader(m_id, m_vertex_id);
+    glAttachShader(m_id, m_frag_id);
+
+    glLinkProgram(m_id);
+
+    glDetachShader(m_id, m_vertex_id);
+    glDetachShader(m_id, m_frag_id);
+
+    // Clean up our now redundant shaders.
+    glDeleteShader(m_vertex_id);
+    glDeleteShader(m_frag_id);
+    m_vertex_id = 0;
+    m_frag_id   = 0;
+
+    // Get the result of linking.
+    GLint status = 0;
+    glGetProgramiv(m_id, GL_LINK_STATUS, &status);
+    m_is_linked = (status == GL_TRUE);
+
+    // If we failed to link, get info log and then fail.
+    if (!m_is_linked) {
+        GLint max_length = 0;
+        glGetProgramiv(m_id, GL_INFO_LOG_LENGTH, &max_length);
+
+        char* log = new char[max_length + 1];
+        glGetProgramInfoLog(m_id, max_length, nullptr, log);
+        log[max_length] = '\0';
+
+        // TODO(Matthew): Send out log somehow (event?).
+
+        return ShaderLinkResult::LINK_FAIL;
+    }
+
+    return ShaderLinkResult::SUCCESS;
+}
+
+bool hg::GLSLProgram::set_attribute(const std::string& name, GLuint index) {
+    return set_attribute({name, index});
+}
+
+bool hg::GLSLProgram::set_attribute(const ShaderAttribute& attribute) {
+    if (!editable()) return false;
+
+    glBindAttribLocation(m_id, attribute.second, attribute.first.data());
+    m_attributes[attribute.first] = attribute.second;
+
+    return true;
+}
+
+bool hg::GLSLProgram::set_attributes(const ShaderAttributes& attributes) {
+    if (!editable()) return false;
+
+    for (auto& attribute : attributes) {
+        glBindAttribLocation(m_id, attribute.second, attribute.first.data());
+        m_attributes[attribute.first] = attribute.second;
+    }
+
+    return true;
+}
+
+GLuint hg::GLSLProgram::uniform_location(const std::string& name) const {
+    // Cannot find location of uniform until the program has been linked.
+    if (!linked()) return GL_INVALID_OPERATION;
+
+    return glGetUniformLocation(m_id, name.data());
+}
+
+void hg::GLSLProgram::enable_vertex_attrib_arrays() const {
+    for (auto& attribute : m_attributes) {
+        glEnableVertexAttribArray(attribute.second);
+    }
+}
+
+void hg::GLSLProgram::disable_vertex_attrib_arrays() const {
+    for (auto& attribute : m_attributes) {
+        glDisableVertexAttribArray(attribute.second);
+    }
+}
+
+bool hg::GLSLProgram::enable_vertex_attrib_array(const std::string& name) const {
+    try {
+        glEnableVertexAttribArray(m_attributes.at(name));
+    } catch (std::out_of_range& e) {
+        return false;
+    }
+    return true;
+}
+
+bool hg::GLSLProgram::disable_vertex_attrib_array(const std::string& name) const {
+    try {
+        glDisableVertexAttribArray(m_attributes.at(name));
+    } catch (std::out_of_range& e) {
+        return false;
+    }
+    return true;
+}
+
+void hg::GLSLProgram::use() {
+    if (!in_use()) {
+        glUseProgram(m_id);
+        GLSLProgram::current = m_id;
+    }
+}
+
+void hg::GLSLProgram::unuse() {
+    if (GLSLProgram::current != 0) {
+        glUseProgram(0);
+        GLSLProgram::current = 0;
+    }
+}
