ea78139bf9e172ce1e179b519fb23da2a960146a
Matthew Marshall <matthew.marshall090396@gmail.com>
2022-02-12 19:23:57 +0000

    Add mesh task following on from load task.

--- src/voxel/loader.cpp
+++ src/voxel/loader.cpp
@@ -2,15 +2,20 @@
 
 #include "voxel/block.hpp"
 #include "voxel/chunk.h"
+#include "voxel/mesher.h"
 
 #include "voxel/loader.h"
 
-void hvox::ChunkLoadTask::execute(ChunkGenThreadState*, ChunkGenTaskQueue*) {
+void hvox::ChunkLoadTask::execute(ChunkGenThreadState*, ChunkGenTaskQueue* task_queue) {
     BlockWorldPosition chunk_position = block_world_position(m_chunk->position, 0);
 
     if (chunk_position.y < 0) {
         set_blocks(m_chunk, BlockChunkPosition{0}, BlockChunkPosition{CHUNK_SIZE}, Block{1});
     }
+
+    ChunkMeshTask* mesh_task = new ChunkMeshTask();
+    mesh_task->init(m_chunk);
+    task_queue->enqueue({ mesh_task, true });
 
     // for (auto x = 0; x < CHUNK_SIZE; ++x) {
     //     for (auto z = 0; z < CHUNK_SIZE; ++z) {
