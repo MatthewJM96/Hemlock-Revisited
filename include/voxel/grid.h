bf3166d997974bfdf4e38fe99701f6135ca0b0ef
Matthew Marshall <matthew.marshall090396@gmail.com>
2022-02-13 19:43:25 +0000

    Introduce preloading of chunks, add checks before going ahead with a load request, and implement chunk-querying functions.

--- include/voxel/grid.h
+++ include/voxel/grid.h
@@ -11,27 +11,228 @@ namespace hemlock {
 
         using Chunks = std::unordered_map<ChunkID, Chunk*>;
 
+        using QueriedChunkState       = std::pair<bool, bool>;
+        using QueriedChunkPendingTask = std::pair<bool, bool>;
+
         class ChunkGrid {
         public:
             ChunkGrid()  { /* Empty. */ }
             ~ChunkGrid() { /* Empty. */ }
 
+            /**
+             * @brief Initialises the chunk grid and the
+             * underlying thread pool.
+             *
+             * @param thread_count The number of threads
+             * that the grid can use for loading tasks.
+             */
             void init(ui32 thread_count);
+            /**
+             * @brief Disposes of the chunk grid, ending
+             * the tasks on the thread pool and unloading
+             * all chunks.
+             */
             void dispose();
 
+            /**
+             * @brief Update loop for chunks.
+             *
+             * @param time The time data for the frame.
+             */
             void update(TimeData time);
 
+            /**
+             * @brief Suspends chunk tasks. This is a hammer, but
+             * for testing it can definitely be useful. Probably
+             * don't ever call this in practise.
+             */
             void suspend_chunk_tasks() { m_gen_threads.suspend(); }
+            /**
+             * @brief Resumes chunk tasks. No consequences for
+             * calling this when not already suspended.
+             */
             void resume_chunk_tasks()  { m_gen_threads.resume();  }
 
+            /**
+             * @brief Loads chunks with the assumption none specified
+             * have even been preloaded. This is useful as it assures
+             * all preloading is done before any loading so that there
+             * is no need for corrective load tasks later for adjoining
+             * chunks etc.
+             *
+             * @param chunk_positions The array of chunk coords for which
+             * to load chunks.
+             * @param chunk_count The number of chunks to load.
+             * @return True if all chunks have got to the point of their
+             * load tasks being queued in a valid state, false if any single
+             * chunk did not.
+             */
+            bool load_from_scratch_chunks(ChunkGridPosition* chunk_positions, ui32 chunk_count);
+
+            /**
+             * @brief Preloads a chunk, this entails saying it exists
+             * and determining its neighbours - letting it and them
+             * know if each other's existence.
+             *
+             * @param chunk_position The coords of the chunk to preload.
+             * @return True if the chunk was preloaded, false otherwise.
+             * False usually will mean that the chunk was at least already
+             * in a preloaded state.
+             */
+            bool preload_chunk_at(ChunkGridPosition chunk_position);
+            /**
+             * @brief Loads a chunk, this entails queuing a generation
+             * task which will, by default, queue a meshing task to
+             * follow up.
+             *
+             * @param chunk_position The coords of the chunk to load.
+             * @return True if the chunk's load task was queued, false
+             * otherwise. False usually will mean that the chunk was
+             * either not yet preloaded, or at least already in a loaded
+             * state.
+             */
             bool load_chunk_at(ChunkGridPosition chunk_position);
+            /**
+             * @brief Unloads a chunk, this entails ending all
+             * pending tasks for this chunk and releasing memory
+             * associated with it.
+             *
+             * @param chunk_position The coords of the chunk to unload.
+             * @return True if the chunk was unloaded, false otherwise.
+             * False usually will mean that the chunk was not yet
+             * existent, as if it is in any existing state some degree
+             * of work will be done to unload it.
+             */
             bool unload_chunk_at(ChunkGridPosition chunk_position);
+
+            /**
+             * @brief Queries the state of the chunk at the given
+             * position. The requirement verified here is that
+             * the so-positioned chunk is at the very least in
+             * the specified state; "later" states shall also
+             * satisfy the requirement here.
+             *
+             * @param chunk_position The position of the chunk
+             * to query.
+             * @param required_minimum_state The minimum state
+             * required of the chunk.
+             * @return [true, true] if the chunk is at least in the
+             * required state, [true, false] if the chunk exists
+             * but does not satisfy the state requirement,
+             * [false, false] if the chunk does not exist. Note:
+             * [false, true] should never occur and represents
+             * invalid query processing.
+             */
+            QueriedChunkState query_chunk_state(ChunkGridPosition chunk_position, ChunkState required_minimum_state);
+            /**
+             * @brief Queries the state of the chunk. The
+             * requirement verified here is that the chunk is
+             * at the very least in the specified state;
+             * "later" states shall also satisfy the
+             * requirement here.
+             *
+             * @param chunk The chunk to query
+             * @param required_minimum_state The minimum state
+             * required of the chunk.
+             * @return [true, true] if the chunk is at least in the
+             * required state, [true, false] if the chunk exists
+             * but does not satisfy the state requirement,
+             * [false, false] if the chunk does not exist. Note:
+             * [false, true] should never occur and represents
+             * invalid query processing.
+             */
+            QueriedChunkState query_chunk_state(Chunk* chunk, ChunkState required_minimum_state);
+
+            /**
+             * @brief Queries the pending task of the chunk
+             * at the given position. The requirement verified
+             * here is that the so-positioned chunk is at the
+             * very least pending going into the specified
+             * task; "later" tasks shall also satisfy the
+             * requirement here as that implies the task
+             * has already been achieved.
+             *
+             * @param chunk_position The position of the chunk
+             * to query.
+             * @param required_minimum_pending_task The minimum
+             * task required of the chunk.
+             * @return [true, true] if the chunk is at least
+             * pending the required task, [true, false] if the
+             * chunk exists but does not satisfy the pending
+             * task requirement, [false, false] if the chunk
+             * does not exist. Note: [false, true] should
+             * never occur and represents invalid query
+             * processing.
+             */
+            QueriedChunkPendingTask query_chunk_pending_task(ChunkGridPosition chunk_position, ChunkLoadTaskKind required_minimum_pending_task);
+            /**
+             * @brief Queries the pending task of the chunk
+             *  The requirement verified here is that the
+             * chunk is at the very least pending going into
+             * the specified task; "later" tasks shall also
+             * satisfy the requirement here as that implies
+             * the task has already been achieved.
+             *
+             * @param chunk The chunk to query
+             * @param required_minimum_pending_task The minimum
+             * task required of the chunk.
+             * @return [true, true] if the chunk is at least
+             * pending the required task, [true, false] if the
+             * chunk exists but does not satisfy the pending
+             * task requirement, [false, false] if the chunk
+             * does not exist. Note: [false, true] should
+             * never occur and represents invalid query
+             * processing.
+             */
+            QueriedChunkPendingTask query_chunk_pending_task(Chunk* chunk, ChunkLoadTaskKind required_minimum_pending_task);
+
+            /**
+             * @brief Queries the state of the neighbours of
+             * the chunk at the given position. The requirement
+             * verified here is that the neighbours of the
+             * so-positioned chunk are at the very least in
+             * the specified state; "later" states shall also
+             * satisfy the requirement here.
+             *
+             * @param chunk_position The position of the chunk
+             * whose neighbours are to be queried.
+             * @param required_minimum_state The minimum state
+             * required of the chunks.
+             * @return [true, true] if the chunks are at least in the
+             * required state, [true, false] if the chunk whose
+             * neighbours we are querying exists but its neighbours
+             * do not satisfy the state requirement, [false, false]
+             * if the chunk whose neighbours we are querying does
+             * not exist. Note: [false, true] should never occur
+             * and represents invalid query processing.
+             */
+            QueriedChunkState query_all_neighbour_states(ChunkGridPosition chunk_position, ChunkState required_minimum_state);
+            /**
+             * @brief Queries the state of the neighbours of
+             * the chunk. The requirement verified here is
+             * that the neighbours of the chunk are at the
+             * very least in the specified state; "later"
+             * states shall also satisfy the requirement here.
+             *
+             * @param chunk The chunk whose neighbours are to
+             * be queried.
+             * @param required_minimum_state The minimum state
+             * required of the chunks.
+             * @return [true, true] if the chunks are at least in the
+             * required state, [true, false] if the chunk whose
+             * neighbours we are querying exists but its neighbours
+             * do not satisfy the state requirement, [false, false]
+             * if the chunk whose neighbours we are querying does
+             * not exist. Note: [false, true] should never occur
+             * and represents invalid query processing.
+             */
+            QueriedChunkState query_all_neighbour_states(Chunk* chunk, ChunkState required_minimum_state);
 
             const Chunks& chunks() { return m_chunks; }
         protected:
             void establish_chunk_neighbours(Chunk* chunk);
 
-            ThreadPool<ChunkGenTaskContext> m_gen_threads;
+            ThreadPool<ChunkLoadTaskContext> m_gen_threads;
 
             Chunks m_chunks;
         };
--- src/voxel/grid.cpp
+++ src/voxel/grid.cpp
@@ -1,8 +1,13 @@
 #include "stdafx.h"
 
-#include "voxel/loader.h"
+#include "voxel/generator.h"
 
 #include "voxel/grid.h"
+
+void hvox::ChunkLoadTask::init(Chunk* chunk, ChunkGrid* chunk_grid) {
+    m_chunk      = chunk;
+    m_chunk_grid = chunk_grid;
+}
 
 void hvox::ChunkGrid::init(ui32 thread_count) {
     m_gen_threads.init(thread_count);
@@ -18,7 +23,21 @@ void hvox::ChunkGrid::update(TimeData time) {
     }
 }
 
-bool hvox::ChunkGrid::load_chunk_at(ChunkGridPosition chunk_position) {
+bool hvox::ChunkGrid::load_from_scratch_chunks(ChunkGridPosition* chunk_positions, ui32 chunk_count) {
+    bool any_chunk_failed = false;
+
+    for (ui32 i = 0; i < chunk_count; ++i) {
+        any_chunk_failed = any_chunk_failed || preload_chunk_at(chunk_positions[i]);
+    }
+
+    for (ui32 i = 0; i < chunk_count; ++i) {
+        any_chunk_failed = any_chunk_failed || load_chunk_at(chunk_positions[i]);
+    }
+
+    return any_chunk_failed;
+}
+
+bool hvox::ChunkGrid::preload_chunk_at(ChunkGridPosition chunk_position) {
     auto it = m_chunks.find(chunk_position.id);
     if (it != m_chunks.end()) return false;
 
@@ -42,9 +61,51 @@ bool hvox::ChunkGrid::load_chunk_at(ChunkGridPosition chunk_position) {
 
     m_chunks[chunk_position.id] = chunk;
 
-    auto task = new ChunkLoadTask();
-    task->init(chunk);
+    return true;
+}
+
+bool hvox::ChunkGrid::load_chunk_at(ChunkGridPosition chunk_position) {
+    auto it = m_chunks.find(chunk_position.id);
+    if (it == m_chunks.end()) return false;
+
+    Chunk* chunk = (*it).second;
+
+    // Note the order of these checks is probably important.
+    // I have guessed for now but expect if we queried pending
+    // after actual state we could encounter race conditions
+    // where the chunk was generated just as we were going
+    // from the first to the second query.
+    //   In this order, we should get no race condition
+    //   as submitting generation tasks should be only
+    //   done from the main thread. That said, we have
+    //   to be careful that we also appropriately update
+    //   these states in the tasks.
+
+    // If chunk is in the process of being generated, we don't
+    // need to add it to the queue again.
+    auto [ _1, chunk_pending_generation ] =
+            query_chunk_pending_task(chunk, ChunkLoadTaskKind::GENERATION);
+    if (chunk_pending_generation)
+        return false;
+
+    // If chunk is already generated, we don't need to try
+    // to generate it again.
+    auto [ _2, chunk_already_generated ] =
+            query_chunk_state(chunk, ChunkState::GENERATED);
+    if (chunk_already_generated)
+        return false;
+
+    // If chunk is not yet preloaded, we can't try to
+    // generate it.
+    auto [ _3, chunk_preloaded ] =
+            query_chunk_state(chunk, ChunkState::PRELOADED);
+    if (!chunk_preloaded)
+        return false;
+
+    auto task = new ChunkGenerationTask();
+    task->init(chunk, this);
     m_gen_threads.add_task({ task, true });
+    chunk->pending_task.store(ChunkLoadTaskKind::GENERATION, std::memory_order_release);
 
     return true;
 }
@@ -52,6 +113,10 @@ bool hvox::ChunkGrid::load_chunk_at(ChunkGridPosition chunk_position) {
 bool hvox::ChunkGrid::unload_chunk_at(ChunkGridPosition chunk_position) {
     auto it = m_chunks.find(chunk_position.id);
     if (it == m_chunks.end()) return false;
+
+    // TODO(Matthew): Should check for pending tasks, and wait on them before unloading.
+    // TODO(Matthew): Maybe we want to even add an unload task so things can hook into this.
+    //                    But then tbh we probably want to just use an event for this.
 
     (*it).second->dispose();
     delete (*it).second;
@@ -130,4 +195,56 @@ void hvox::ChunkGrid::establish_chunk_neighbours(Chunk* chunk) {
     } else {
         chunk->neighbours.back = nullptr;
     }
+}
+
+hvox::QueriedChunkState hvox::ChunkGrid::query_chunk_state(ChunkGridPosition chunk_position, ChunkState required_minimum_state) {
+    auto it = m_chunks.find(chunk_position.id);
+    if (it == m_chunks.end()) return {false, false};
+
+    return query_chunk_state((*it).second, required_minimum_state);
+}
+
+hvox::QueriedChunkState hvox::ChunkGrid::query_chunk_state(Chunk* chunk, ChunkState required_minimum_state) {
+    if (chunk == nullptr) return {false, false};
+
+    ChunkState actual_state = chunk->state.load(std::memory_order_acquire);
+
+    return {true, actual_state >= required_minimum_state};
+}
+
+hvox::QueriedChunkPendingTask hvox::ChunkGrid::query_chunk_pending_task(ChunkGridPosition chunk_position, ChunkLoadTaskKind required_minimum_pending_task) {
+    auto it = m_chunks.find(chunk_position.id);
+    if (it == m_chunks.end()) return {false, false};
+
+    return query_chunk_pending_task((*it).second, required_minimum_pending_task);
+}
+
+hvox::QueriedChunkPendingTask hvox::ChunkGrid::query_chunk_pending_task(Chunk* chunk, ChunkLoadTaskKind required_minimum_pending_task) {
+    if (chunk == nullptr) return {false, false};
+
+    ChunkLoadTaskKind actual_pending_task = chunk->pending_task.load(std::memory_order_acquire);
+
+    return {true, actual_pending_task >= required_minimum_pending_task};
+}
+
+hvox::QueriedChunkState hvox::ChunkGrid::query_all_neighbour_states(ChunkGridPosition chunk_position, ChunkState required_minimum_state) {
+    auto it = m_chunks.find(chunk_position.id);
+    if (it == m_chunks.end()) return {false, false};
+
+    return query_all_neighbour_states((*it).second, required_minimum_state);
+}
+
+hvox::QueriedChunkState hvox::ChunkGrid::query_all_neighbour_states(Chunk* chunk, ChunkState required_minimum_state) {
+    if (chunk == nullptr) return {false, false};
+
+    bool all_neighbours_satisfy_constraint = true;
+    for (ui32 i = 0; i < 8; ++i) {
+        if (chunk->neighbours.neighbours[i] == nullptr) continue;
+
+        ChunkState actual_state = chunk->neighbours.neighbours[i]->state.load(std::memory_order_acquire);
+
+        all_neighbours_satisfy_constraint = all_neighbours_satisfy_constraint && (actual_state >= required_minimum_state);
+    }
+
+    return {true, all_neighbours_satisfy_constraint};
 }
