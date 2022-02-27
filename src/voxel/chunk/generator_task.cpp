cb78a7b8db395f49c8e420384b27c50394818d22
Matthew Marshall <matthew.marshall090396@gmail.com>
2022-02-27 00:09:06 +0000

    Revert to using naive meshing method for now and move on to better things.

--- src/voxel/chunk/generator_task.cpp
+++ src/voxel/chunk/generator_task.cpp
@@ -2,7 +2,8 @@
 
 #include "voxel/block.hpp"
 #include "voxel/chunk.h"
-#include "voxel/chunk/mesh/greedy_task.h"
+// #include "voxel/chunk/mesh/greedy_task.h"
+#include "voxel/chunk/mesh/naive_task.h"
 
 #include "voxel/chunk/generator_task.h"
 
@@ -18,7 +19,8 @@ void hvox::ChunkGenerationTask::execute(ChunkLoadThreadState* state, ChunkLoadTa
 
     m_chunk->state.store(ChunkState::GENERATED, std::memory_order_release);
 
-    ChunkGreedyMeshTask* mesh_task = new ChunkGreedyMeshTask();
+    // ChunkGreedyMeshTask* mesh_task = new ChunkGreedyMeshTask();
+    ChunkNaiveMeshTask* mesh_task = new ChunkNaiveMeshTask();
     mesh_task->init(m_chunk, m_chunk_grid, nullptr);
     m_chunk->pending_task.store(ChunkLoadTaskKind::MESH, std::memory_order_release);
     task_queue->enqueue(state->producer_token, { mesh_task, true });
