1b189abbc711aa32cac544f57954062e4ca0a468
Matthew Marshall <matthew.marshall090396@gmail.com>
2022-03-08 00:35:24 +0000

    Create start of chunk load/save tasks.

---
+++ include/voxel/io/chunk_file_task.h
@@ -0,0 +1,28 @@
+#ifndef __hemlock_voxel_io_chunk_file_task_hpp
+#define __hemlock_voxel_io_chunk_file_task_hpp
+
+#include "io/io_task.hpp"
+
+namespace hemlock {
+    namespace voxel {
+        struct Chunk;
+
+        using ChunkFileTaskThreadState = io::IOTaskThreadState;
+        using ChunkFileTaskTaskQueue   = io::IOTaskTaskQueue;
+
+        class ChunkFileTask : public io::IOTask {
+        public:
+            void init(Chunk* chunk, io::IOManagerBase* iomanager) {
+                io::IOTask::init(iomanager);
+
+                m_chunk = chunk;
+            }
+        protected:
+            Chunk* m_chunk;
+            static hio::fs::mapped_file chunk_data_file;
+        };
+    }
+}
+namespace hvox = hemlock::voxel;
+
+#endif // __hemlock_voxel_io_chunk_file_task_hpp
---
+++ include/voxel/io/chunk_save_task.h
@@ -0,0 +1,16 @@
+#ifndef __hemlock_voxel_io_chunk_save_task_h
+#define __hemlock_voxel_io_chunk_save_task_h
+
+#include "voxel/io/chunk_file_task.h"
+
+namespace hemlock {
+    namespace voxel {
+        class ChunkSaveTask : public ChunkFileTask {
+        public:
+            virtual void execute(ChunkFileTaskThreadState* state, ChunkFileTaskTaskQueue* task_queue) override;
+        };
+    }
+}
+namespace hvox = hemlock::voxel;
+
+#endif // __hemlock_voxel_io_chunk_save_task_h
