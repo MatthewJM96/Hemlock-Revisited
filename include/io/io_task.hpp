dea064b286f9863e9736c42e557b4cfcfd5f9fe5
Matthew Marshall <matthew.marshall090396@gmail.com>
2022-03-08 00:34:46 +0000

    Define a basic IO task.

---
+++ include/io/io_task.hpp
@@ -0,0 +1,23 @@
+#ifndef __hemlock_io_io_task_hpp
+#define __hemlock_io_io_task_hpp
+
+namespace hemlock {
+    namespace io {
+        class IOManagerBase;
+
+        using IOTaskThreadState = thread::Thread<thread::BasicThreadContext>::State;
+        using IOTaskTaskQueue   = thread::TaskQueue<thread::BasicThreadContext>;
+
+        class IOTask : public thread::IThreadTask<thread::BasicThreadContext> {
+        public:
+            void init(IOManagerBase* iomanager) {
+                m_iomanager = iomanager;
+            }
+        protected:
+            IOManagerBase* m_iomanager;
+        };
+    }
+}
+namespace hio = hemlock::io;
+
+#endif // __hemlock_io_io_task_hpp
