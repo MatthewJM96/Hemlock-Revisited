406332fe6c61632db8998b6c5fa7f97c64dab513
Matthew Marshall <matthew.marshall090396@gmail.com>
2022-06-19 23:49:59 +0100

    Little bit of prep for implementing ray.

--- src/voxel/ray.cpp
+++ src/voxel/ray.cpp
@@ -2,3 +2,38 @@
 
 #include "voxel/ray.h"
 
+bool hvox::Ray::cast_to_block(      f32v3 start, 
+                                    f32v3 direction,
+              hmem::WeakHandle<ChunkGrid> chunk_handle,
+                                    Block target_block,
+                                     ui32 max_steps,
+                  OUT BlockWorldPosition& position,
+                                 OUT f32& distance      )
+{
+    return cast_to_block(
+        start,
+        direction,
+        chunk_handle,
+        {[target_block](const Block& test) { return target_block == test; }},
+        max_steps,
+        position,
+        distance
+    );
+}
+
+bool hvox::Ray::cast_to_block(      f32v3 start,
+                                    f32v3 direction,
+              hmem::WeakHandle<ChunkGrid> chunk_handle,
+                                BlockTest block_is_target,
+                                     ui32 max_steps,
+                  OUT BlockWorldPosition& position,
+                                 OUT f32& distance      )
+{
+    auto chunk_grid = chunk_handle.lock();
+
+    if (chunk_grid == nullptr) return false;
+
+    // TODO(Matthew): implement
+
+    return false;
+}
