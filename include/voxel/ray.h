a21cbaf2477cba1ae1169c091467773638f173c7
Matthew Marshall <matthew.marshall090396@gmail.com>
2022-06-21 18:25:24 +0100

    Add cast_to_block_before and introduce step_to_next_block_position to use in all these.

--- include/voxel/ray.h
+++ include/voxel/ray.h
@@ -16,7 +16,7 @@ namespace hemlock {
              * first block that is exactly the specified target block.
              *
              * @param start The location the ray starts at.
-             * @param direction The direction to cast the ray in.
+             * @param direction The normalised direction to cast the ray in.
              * @param grid_handle Handle to the chunk grid in which the ray is cast.
              * @param target_block The target block that the ray will stop upon
              * reaching.
@@ -44,7 +44,7 @@ namespace hemlock {
              * block_is_target.
              *
              * @param start The location the ray starts at.
-             * @param direction The direction to cast the ray in.
+             * @param direction The normalised direction to cast the ray in.
              * @param grid_handle Handle to the chunk grid in which the ray is cast.
              * @param block_is_target The test function, must return false
              * except for a block considered a target of the ray.
@@ -58,6 +58,63 @@ namespace hemlock {
              * target within max_steps, false otherwise.
              */
             bool cast_to_block(           f32v3 start,
+                                          f32v3 direction,
+                    hmem::WeakHandle<ChunkGrid> grid_handle,
+                                      BlockTest block_is_target,
+                                           ui32 max_steps,
+                        OUT BlockWorldPosition& position,
+                                       OUT f32& distance    );
+
+            /**
+             * @brief Steps the ray along its direction until the
+             * first block that is exactly the specified target block.
+             * The position and distance are set to the block the ray
+             * was incident on before that block.
+             *
+             * @param start The location the ray starts at.
+             * @param direction The normalised direction to cast the ray in.
+             * @param grid_handle Handle to the chunk grid in which the ray is cast.
+             * @param target_block The target block that the ray will stop upon
+             * reaching.
+             * @param max_steps The maximum number of steps the ray should take
+             * before failing.
+             * @param position The position of the block the ray met, unchanged
+             * if the ray was not incident on the target block.
+             * @param distance The distance to the block the ray met, unchanged
+             * if the ray was not incident on the target block.
+             * @return True if the ray was incident on a block matching the
+             * target within max_steps, false otherwise.
+             */
+            bool cast_to_block_before(    f32v3 start,
+                                          f32v3 direction,
+                    hmem::WeakHandle<ChunkGrid> grid_handle,
+                                          Block target_block,
+                                           ui32 max_steps,
+                        OUT BlockWorldPosition& position,
+                                       OUT f32& distance    );
+
+
+            /**
+             * @brief Steps the ray along its direction until the
+             * first block that satisfies the conditions encoded by
+             * block_is_target. The position and distance are set to
+             * the block the ray was incident on before that block.
+             *
+             * @param start The location the ray starts at.
+             * @param direction The normalised direction to cast the ray in.
+             * @param grid_handle Handle to the chunk grid in which the ray is cast.
+             * @param block_is_target The test function, must return false
+             * except for a block considered a target of the ray.
+             * @param max_steps The maximum number of steps the ray should take
+             * before failing.
+             * @param position The position of the block the ray met, unchanged
+             * if the ray was not incident on the target block.
+             * @param distance The distance to the block the ray met, unchanged
+             * if the ray was not incident on the target block.
+             * @return True if the ray was incident on a block matching the
+             * target within max_steps, false otherwise.
+             */
+            bool cast_to_block_before(    f32v3 start,
                                           f32v3 direction,
                     hmem::WeakHandle<ChunkGrid> grid_handle,
                                       BlockTest block_is_target,
--- src/voxel/ray.cpp
+++ src/voxel/ray.cpp
@@ -1,6 +1,53 @@
 #include "stdafx.h"
 
+#include "voxel/chunk/grid.h"
 #include "voxel/ray.h"
+
+inline static void step_to_next_block_position(IN OUT f32v3& position, f32v3 direction, OUT f32& distance) {
+    // Assume direction here is normalised, blocks are defined on the integer boundaries.
+    // TODO(Matthew): I think the way block is defined, and the meshers work, blocks are rendered on half-integer boundaries.
+
+    distance = 0.0f;
+
+    f32 min_orthogonal_weighting = std::numeric_limits<f32>::max();
+    f32 candidate_orthogonal_weighting = 0.0f;
+
+    // X
+    if (direction.x < 0.0f) {
+        min_orthogonal_weighting = (glm::floor(position.x) - position.x) / direction.x;
+    } else if (direction.x > 0.0f) {
+        min_orthogonal_weighting = (glm::ceil(position.x) - position.x) / direction.x;
+    }
+
+
+    // Y
+    if (direction.y < 0.0f) {
+        candidate_orthogonal_weighting = (glm::floor(position.y) - position.y) / direction.y;
+
+        if (candidate_orthogonal_weighting < min_orthogonal_weighting) min_orthogonal_weighting = candidate_orthogonal_weighting;
+    } else if (direction.y > 0.0f) {
+        candidate_orthogonal_weighting = (glm::ceil(position.y) - position.y) / direction.y;
+
+        if (candidate_orthogonal_weighting < min_orthogonal_weighting) min_orthogonal_weighting = candidate_orthogonal_weighting;
+    }
+
+    // Z
+    if (direction.z < 0.0f) {
+        candidate_orthogonal_weighting = (glm::floor(position.z) - position.z) / direction.z;
+
+        if (candidate_orthogonal_weighting < min_orthogonal_weighting) min_orthogonal_weighting = candidate_orthogonal_weighting;
+    } else if (direction.z > 0.0f) {
+        candidate_orthogonal_weighting = (glm::ceil(position.z) - position.z) / direction.z;
+
+        if (candidate_orthogonal_weighting < min_orthogonal_weighting) min_orthogonal_weighting = candidate_orthogonal_weighting;
+    }
+
+    f32v3 old_position = position;
+    position += min_orthogonal_weighting * direction;
+
+    f32v3 move = old_position - position;
+    distance = glm::sqrt(glm::dot(move, move));
+}
 
 bool hvox::Ray::cast_to_block(      f32v3 start, 
                                     f32v3 direction,
@@ -28,6 +75,49 @@ bool hvox::Ray::cast_to_block(      f32v3 start,
                                      ui32 max_steps,
                   OUT BlockWorldPosition& position,
                                  OUT f32& distance      )
+{
+    auto chunk_grid = chunk_handle.lock();
+
+    if (chunk_grid == nullptr) return false;
+
+    // TODO(Matthew): implement
+
+    (void)start;
+    (void)direction;
+    (void)block_is_target;
+    (void)max_steps;
+    (void)position;
+    (void)distance;
+
+    return false;
+}
+
+bool hvox::Ray::cast_to_block_before( f32v3 start, 
+                                      f32v3 direction,
+                hmem::WeakHandle<ChunkGrid> chunk_handle,
+                                      Block target_block,
+                                       ui32 max_steps,
+                    OUT BlockWorldPosition& position,
+                                   OUT f32& distance      )
+{
+    return cast_to_block_before(
+        start,
+        direction,
+        chunk_handle,
+        {[target_block](const Block& test) { return target_block == test; }},
+        max_steps,
+        position,
+        distance
+    );
+}
+
+bool hvox::Ray::cast_to_block_before( f32v3start,
+                                      f32v3 direction,
+                hmem::WeakHandle<ChunkGrid> chunk_handle,
+                                  BlockTest block_is_target,
+                                       ui32 max_steps,
+                    OUT BlockWorldPosition& position,
+                                   OUT f32& distance      )
 {
     auto chunk_grid = chunk_handle.lock();
 
