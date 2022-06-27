e2f23a786a3e8126ddb7634c95a8e7c3574a9402
Matthew Marshall <matthew.marshall090396@gmail.com>
2022-06-27 22:29:20 +0100

    Add missing file to redefine generic chunk task.

---
+++ include/voxel/chunk/task.hpp
@@ -0,0 +1,33 @@
+#ifndef __hemlock_voxel_chunk_load_task_hpp
+#define __hemlock_voxel_chunk_load_task_hpp
+
+namespace hemlock {
+    namespace voxel {
+        struct Chunk;
+        class ChunkGrid;
+
+        enum class ChunkTaskKind : ui8 {
+            NONE        = 0,
+            GENERATION     ,
+            MESH           ,
+            MESH_UPLOAD    ,
+        };
+
+        using ChunkTaskContext      = thread::BasicThreadContext;
+        using ChunkLoadThreadState  = thread::Thread<ChunkTaskContext>::State;
+        using ChunkTaskQueue        = thread::TaskQueue<ChunkTaskContext>;
+
+        class ChunkTask : public thread::IThreadTask<ChunkTaskContext> {
+        public:
+            virtual ~ChunkTask() { /* Empty. */ }
+
+            void set_state(hmem::WeakHandle<Chunk> chunk, hmem::WeakHandle<ChunkGrid> chunk_grid);
+        protected:
+            hmem::WeakHandle<Chunk>     m_chunk;
+            hmem::WeakHandle<ChunkGrid> m_chunk_grid;
+        };
+    }
+}
+namespace hvox = hemlock::voxel;
+
+#endif // __hemlock_voxel_chunk_load_task_hpp
