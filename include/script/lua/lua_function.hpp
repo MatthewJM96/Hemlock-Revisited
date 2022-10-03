0b298684942e7178483102db724433ead12f096b
Matthew Marshall <matthew.marshall090396@gmail.com>
2022-10-03 21:19:04 +0100

    Provide means of making a delegate that provides call access to Lua functions.

--- include/script/lua/lua_function.hpp
+++ include/script/lua/lua_function.hpp
@@ -17,9 +17,37 @@ namespace hemlock {
                  */
                 i32 register_lua_function(LuaHandle state);
 
+                /**
+                 * @brief Deregisters the given lua function, removing it
+                 * from the environment's registry table.
+                 *
+                 * @param state The Lua state to deregister within.
+                 * @param lua_func_state The Lua function to deregister.
+                 */
+                void deregister_lua_function(LuaHandle state, LuaFunctionState lua_func_state);
+
+                /**
+                 * @brief Creates a delegate for calling the given
+                 * Lua function, handling errors and so value passing.
+                 *
+                 * Note that the delegate itself returns a tuple of a
+                 * bool and the ReturnType specified, the bool indicating
+                 * if the Lua function was successfully called.
+                 *
+                 * @tparam ReturnType The return type of the Lua function.
+                 * @tparam Parameters The parameter types to pass to the Lua
+                 * function.
+                 * @param lua_func_state The Lua function.
+                 * @return ScriptDelegate<ReturnType, Parameters...> The
+                 * delegate providing call access to the Lua function.
+                 */
+                template <typename ReturnType, typename ...Parameters>
+                ScriptDelegate<ReturnType, Parameters...> make_lua_delegate(LuaFunctionState lua_func_state);
         }
     }
 }
 namespace hscript = hemlock::script;
+
+#include "lua_function.inl"
 
 #endif // __hemlock_script_lua_lua_function_hpp
--- src/script/lua/lua_function.cpp
+++ src/script/lua/lua_function.cpp
@@ -33,3 +33,14 @@ i32 hscript::lua::register_lua_function(LuaHandle state) {
     LuaValue<i32>::push(state, 0);
     return 1;
 }
+
+void hscript::lua::deregister_lua_function(LuaHandle state, LuaFunctionState lua_func_state) {
+    lua_getfield(state, LUA_REGISTRYINDEX, H_LUA_SCRIPT_FUNCTION_TABLE);
+    luaL_unref(state, -1, lua_func_state.index);
+
+    // NOTE(Matthew): Docs don't say if luaL_unref pops the indexed-into
+    //                object, and it likely doesn't. We pop here, but if
+    //                luaL_unref does in fact pop, then this will cause
+    //                issues.
+    lua_pop(state, 1);
+}
