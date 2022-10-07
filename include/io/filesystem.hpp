106ffb1b88fe2f24585146d8c21f47a80e32cdd8
Matthew Marshall <matthew.marshall090396@gmail.com>
2022-10-07 13:45:33 +0100

    Integrate all symbols in fs namespace rather than in std and then importing that.

--- include/io/filesystem.hpp
+++ include/io/filesystem.hpp
@@ -1,16 +1,18 @@
 #ifndef __hemlock_io_filesystem_hpp
 #define __hemlock_io_filesystem_hpp
 
-// TODO(Matthew): Do we abstract these to hide their open calls?
-namespace std::filesystem {
-    using mapped_file        = boost::iostreams::mapped_file;
-    using mapped_file_source = boost::iostreams::mapped_file_source;
-    using mapped_file_sink   = boost::iostreams::mapped_file_sink;
-};
-
 namespace hemlock {
     namespace io {
-        namespace fs = std::filesystem;
+        namespace fs {
+            using namespace std::filesystem;
+
+            // TODO(Matthew): Do we abstract these to hide their open calls?
+            using mapped_file        = boost::iostreams::mapped_file;
+            using mapped_file_source = boost::iostreams::mapped_file_source;
+            using mapped_file_sink   = boost::iostreams::mapped_file_sink;
+
+            using paths = std::vector<path>;
+        }
     }
 }
 namespace hio = hemlock::io;
