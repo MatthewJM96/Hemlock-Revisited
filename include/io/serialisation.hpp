411ed501680b3a58382a7823f97923ff7c756f94
Matthew Marshall <matthew.marshall090396@gmail.com>
2022-10-09 21:13:07 +0100

    Fix stringising certain components of serialisation macros.

--- include/io/serialisation.hpp
+++ include/io/serialisation.hpp
@@ -28,31 +28,33 @@ enum class NAME {                                                   \
 #endif //!defined(H_DEF_SERIALISABLE_ENUM)
 
 #if !defined(H_DEF_ENUM_SERIALISATION)
-#define H_SERIALISABLE_ENUM_NAME(NAME, VAL) NAME
-#define H_SERIALISABLE_ENUM_VAL(NAME, VAL) { NAME, VAL }
+#define H_ENUM_GET_NAME(NAME, VAL) #NAME
+#define H_ENUM_GET_VAL(NAME, VAL) VAL
+#define H_ENUM_NAME(ENTRY) H_ENUM_GET_NAME ENTRY
+#define H_ENUM_VAL(ENTRY) { H_ENUM_GET_NAME ENTRY, H_ENUM_GET_VAL ENTRY }
 
 #define H_DEF_ENUM_SERIALISATION(NAME, ...)                                     \
 namespace hemlock {                                                             \
     namespace io {                                                              \
-        const char* NAME_Names[] = {                                            \
-            MAP(H_SERIALISABLE_ENUM_NAME, COMMA, __VA_ARGS__)                   \
+        const char* NAME##_Names[] = {                                          \
+            MAP(H_ENUM_NAME, COMMA, __VA_ARGS__)                                \
         };                                                                      \
                                                                                 \
-        const std::unordered_map<std::string, NAME> NAME_Values = {             \
-            MAP(H_SERIALISABLE_ENUM_VAL, COMMA, __VA_ARGS__)                    \
+        const std::unordered_map<std::string, NAME> NAME##_Values = {           \
+            MAP(H_ENUM_VAL, COMMA, __VA_ARGS__)                                 \
         };                                                                      \
                                                                                 \
         template <>                                                             \
-        constexpr const char* serialisable_enum_name<NAME>(NAME val) {          \
-            return NAME_Names[                                                  \
+        const char* serialisable_enum_name<NAME>(NAME val) {                    \
+            return NAME##_Names[                                                \
                 static_cast<size_t>(val)                                        \
             ];                                                                  \
         }                                                                       \
                                                                                 \
         template <>                                                             \
         NAME serialisable_enum_val<NAME>(const std::string& name) {             \
-            auto it = NAME_Vals.find(name);                                     \
-            if (it != NAME_Vals.end()) {                                        \
+            auto it = NAME##_Values.find(name);                                 \
+            if (it != NAME##_Values.end()) {                                    \
                 return it->second;                                              \
             }                                                                   \
             return NAME::SENTINEL;                                              \
