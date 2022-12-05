e3f2f21ce2cd0aa90baffbacdf672574cdbac7a7
Matthew Marshall <matthew.marshall090396@gmail.com>
2022-12-05 20:06:12 +0000

    Call print_heatmap_frames in maze graph test.

--- tests/ai_screen/maze_graph.hpp
+++ tests/ai_screen/maze_graph.hpp
@@ -6,17 +6,15 @@ void __do_maze_graph_test(size_t map_dim, size_t count) {
     for (size_t map_idx = 0; map_idx < count; ++map_idx) {
         map::maze2d::Map map;
 
+        std::string tag = std::to_string(half_dim) + "." + std::to_string(map_idx);
+
         map = map::maze2d::load_map(
-            "maze/" + std::to_string(half_dim) + "." + std::to_string(map_idx)
-                + ".solved.map",
-            { map_dim, map_dim }
+            "maze/" + tag + ".solved.map", { map_dim, map_dim }
         );
 
         std::cout << "Map " << map_idx + 1 << " of dim " << half_dim
                   << " maps, with ideal solution length " << map.solution_length << ":"
                   << std::endl;
-
-        // map::maze2d::print_map(map);
 
         halgo::GraphMap<size_t> graph_map = map::maze2d::map_to_graph(map, 1.0f);
 
@@ -35,9 +33,13 @@ void __do_maze_graph_test(size_t map_dim, size_t count) {
 
         size_t* path        = nullptr;
         size_t  path_length = 0;
-        acs.find_path<100, 2000>(
+        acs.find_path<20, 100>(
             graph_map, map.start_idx, map.finish_idx, path, path_length
         );
+
+#if defined(DEBUG)
+        acs.print_heatmap_frames<100>(tag);
+#endif  // defined(DEBUG)
 
         std::cout << "  solution found with length: " << path_length << std::endl
                   << std::endl;
