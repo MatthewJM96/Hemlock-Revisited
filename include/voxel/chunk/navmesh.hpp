d608b50b2f4438be376481802bc74a8222dcddac
Matthew Marshall <matthew.marshall090396@gmail.com>
2023-01-21 01:26:30 +0000

    Specify means of storing navmesh for chunks.

---
+++ include/voxel/chunk/navmesh.hpp
@@ -0,0 +1,51 @@
+#ifndef __hemlock_voxel_chunk_navmesh_hpp
+#define __hemlock_voxel_chunk_navmesh_hpp
+
+#include "algorithm/acs/graph/state.hpp"
+#include "voxel/coordinate_system.h"
+
+namespace hemlock {
+    namespace voxel {
+        struct ChunkNavmeshNode {
+            BlockChunkPosition block_pos;
+            ChunkGridPosition  chunk_pos;
+        };
+
+        using ChunkNavmesh = algorithm::GraphMap<ChunkNavmeshNode, false>;
+        using ChunkNavmeshVertexDescriptor
+            = algorithm::VertexDescriptor<ChunkNavmeshNode, false>;
+    }  // namespace voxel
+}  // namespace hemlock
+namespace hvox = hemlock::voxel;
+
+namespace std {
+    template <>
+    struct hash<hvox::ChunkNavmeshNode> {
+        size_t operator()(const hvox::ChunkNavmeshNode& node) const {
+            union {
+                struct {
+                    i64 block_x : 5;
+                    i64 block_y : 5;
+                    i64 block_z : 5;
+                    i64 chunk_x : 19;
+                    i64 chunk_y : 11;
+                    i64 chunk_z : 19;
+                } __attribute__((packed)) coords;
+
+                ui64 id;
+            } packed_node;
+
+            packed_node.coords.block_x = node.block_pos.x;
+            packed_node.coords.block_y = node.block_pos.y;
+            packed_node.coords.block_z = node.block_pos.z;
+
+            packed_node.coords.chunk_x = node.chunk_pos.x;
+            packed_node.coords.chunk_y = node.chunk_pos.y;
+            packed_node.coords.chunk_z = node.chunk_pos.z;
+
+            return packed_node.id;
+        }
+    };
+}  // namespace std
+
+#endif  // __hemlock_voxel_chunk_navmesh_hpp
