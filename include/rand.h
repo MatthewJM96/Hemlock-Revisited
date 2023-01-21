247de4714e15586c2d337058fb9474ea2d1fbbef
Matthew Marshall <matthew.marshall090396@gmail.com>
2023-01-21 00:58:34 +0000

    Implement a general pair of rands for use in future ACS rewrite and elsewhere.

---
+++ include/rand.h
@@ -0,0 +1,12 @@
+#ifndef __hemlock_rand_h
+#define __hemlock_rand_h
+
+namespace hemlock {
+    template <std::floating_point Precision>
+    Precision global_unitary_rand();
+
+    template <std::floating_point Precision>
+    Precision global_rand(Precision min, Precision max);
+}  // namespace hemlock
+
+#endif  // __hemlock_rand_h
