98e82bda0c1a247be1810ded0b25dcc1dd6b64a7
Matthew Marshall <matthew.marshall090396@gmail.com>
2023-03-07 22:34:53 +0000

    Separate out navmesh strategies from the implementation detail of task queues, setting up for easier profiling.

--- include/voxel/ai/navmesh_task.hpp
+++ include/voxel/ai/navmesh_task.hpp
@@ -1,23 +1,36 @@
 #ifndef __hemlock_voxel_ai_navmesh_task_hpp
 #define __hemlock_voxel_ai_navmesh_task_hpp
 
-#include "voxel/predicate.hpp"
 #include "voxel/task.hpp"
 
 namespace hemlock {
     namespace voxel {
+        class ChunkGrid;
         struct Chunk;
 
         namespace ai {
-            template <hvox::IdealBlockConstraint IsSolid>
+            /**
+             * @brief Defines a struct whose opeartor() sets the blocks of a chunk.
+             */
+            template <typename StrategyCandidate>
+            concept ChunkNavmeshStrategy
+                = requires (StrategyCandidate s, hmem::Handle<ChunkGrid> g, hmem::Handle<Chunk> c) {
+                    {
+                        s.do_bulk(g, c)
+                        } -> std::same_as<void>;
+                    {
+                        s.do_stitch(g, c)
+                        } -> std::same_as<void>;
+                };
+
+            template <hvox::ai::ChunkNavmeshStrategy NavmeshStrategy>
             class ChunkNavmeshTask : public ChunkTask {
             public:
                 virtual ~ChunkNavmeshTask() { /* Empty. */
                 }
 
-                virtual void execute(
-                    ChunkLoadThreadState* state, ChunkTaskQueue* task_queue
-                ) override;
+                virtual void
+                execute(ChunkLoadThreadState* state, ChunkTaskQueue* task_queue) override;
             };
         }  // namespace ai
     }      // namespace voxel
