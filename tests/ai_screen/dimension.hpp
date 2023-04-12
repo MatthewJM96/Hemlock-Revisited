d96dec286955de9782aed4ea21698f9fd1096037
Matthew Marshall <matthew.marshall090396@gmail.com>
2023-04-12 16:25:46 +0100

    Deal with some conversions MSVC wasn't liking.

--- tests/ai_screen/dimension.hpp
+++ tests/ai_screen/dimension.hpp
@@ -3,26 +3,26 @@
 
 namespace dimension {
     struct Map2DDimensions {
-        size_t x, y;
+        ui32 x, y;
     };
 
-    constexpr size_t dim_to_padded_dim(size_t dim) {
+    constexpr ui32 dim_to_padded_dim(ui32 dim) {
         return dim + 2;
     }
 
-    constexpr size_t dim2d_to_padded_size(size_t dim) {
+    constexpr ui32 dim2d_to_padded_size(ui32 dim) {
         return dim_to_padded_dim(dim) * dim_to_padded_dim(dim);
     }
 
-    constexpr size_t dim2d_to_padded_size(size_t dim_x, size_t dim_y) {
+    constexpr ui32 dim2d_to_padded_size(ui32 dim_x, ui32 dim_y) {
         return dim_to_padded_dim(dim_x) * dim_to_padded_dim(dim_y);
     }
 
-    constexpr size_t dim3d_to_padded_size(size_t dim) {
+    constexpr ui32 dim3d_to_padded_size(ui32 dim) {
         return dim_to_padded_dim(dim) * dim_to_padded_dim(dim) * dim_to_padded_dim(dim);
     }
 
-    constexpr size_t dim3d_to_padded_size(size_t dim_x, size_t dim_y, size_t dim_z) {
+    constexpr ui32 dim3d_to_padded_size(ui32 dim_x, ui32 dim_y, ui32 dim_z) {
         return dim_to_padded_dim(dim_x) * dim_to_padded_dim(dim_y)
                * dim_to_padded_dim(dim_z);
     }
