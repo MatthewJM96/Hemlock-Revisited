c5471587315f361edb82b19ba30bf2e7ebfc7b65
Matthew Marshall <matthew.marshall090396@gmail.com>
2023-04-12 13:49:48 +0100

    Avoid copy constructor in initialisation of navmesh_stitch.

--- src/voxel/chunk.cpp
+++ src/voxel/chunk.cpp
@@ -12,17 +12,10 @@ hvox::Chunk::Chunk() :
     meshing(ChunkState::NONE),
     mesh_uploading(ChunkState::NONE),
     navmeshing(ChunkState::NONE),
-    navmesh_stitch({ ChunkState::NONE,
-                     ChunkState::NONE,
-                     ChunkState::NONE,
-                     ChunkState::NONE,
-                     ChunkState::NONE,
-                     ChunkState::NONE,
-                     ChunkState::NONE,
-                     ChunkState::NONE,
-                     ChunkState::NONE,
-                     ChunkState::NONE,
-                     ChunkState::NONE }) { /* Empty. */
+    navmesh_stitch{ ChunkState::NONE, ChunkState::NONE, ChunkState::NONE,
+                    ChunkState::NONE, ChunkState::NONE, ChunkState::NONE,
+                    ChunkState::NONE, ChunkState::NONE, ChunkState::NONE,
+                    ChunkState::NONE, ChunkState::NONE } { /* Empty. */
 }
 
 hvox::Chunk::~Chunk() {
