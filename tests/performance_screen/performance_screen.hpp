358a434e9d6970a3c10a7137149fcbc291f15a49
Matthew Marshall <matthew.marshall090396@gmail.com>
2023-04-12 15:30:11 +0100

    Comments on performance in Clang run now we have that (way faster than GCC, strange).

--- tests/performance_screen/performance_screen.hpp
+++ tests/performance_screen/performance_screen.hpp
@@ -25,13 +25,16 @@
 //                for a 20 chunk view distance, in a game using the half-metre per voxel
 //                metric (so 320m view distance), we pessimistically need to prepare
 //                ~7000 chunks per second. With current (Apr 2023) timings, we have a
-//                ~1.5ms per chunk cost, which allows for ~670 chunks a second per
-//                thread. We can expect to target future hardware with 12 cores and
-//                upward, but we'd in those circumstances need to provide 10-11 threads
-//                to chunk preparation alone, ignoring any systems to maintain the
-//                on-vehicle experience and any future additional chunk prep costs.
+//                ~1.4ms per chunk cost, which allows for ~720 chunks a second per
+//                thread on GCC and ~0.9ms for ~1100 chunks a second per thread on
+//                Clang. We can expect to target future hardware with 12 cores and
+//                upward, but we'd in those circumstances need to provide 10 (GCC) or
+//                7 (Clang) threads to chunk preparation alone, ignoring any systems to
+//                maintain the local environment experience and any future additional
+//                chunk prep costs.
 //                  Suffice to say, we would like to bring these timings down closer to
-//                  1ms to give any tricks we would need to employ more slack to work.
+//                  0.7ms (=> 5 threads needed) to give any tricks we would need to
+//                  employ more slack to work.
 
 class TestPerformanceScreen : public happ::ScreenBase {
 public:
