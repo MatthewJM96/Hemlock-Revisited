6d4c40beb066acef79b033262d580b589e4d3b27
Matthew Marshall <matthew.marshall090396@gmail.com>
2023-04-14 17:22:46 +0100

    Finish first-pass implementation of navmesh outline renderer.

--- include/voxel/graphics/outline_renderer/navmesh.h
+++ include/voxel/graphics/outline_renderer/navmesh.h
@@ -8,6 +8,44 @@
 namespace hemlock {
     namespace voxel {
         class NavmeshOutlineRenderer {
+        protected:
+            struct NavmeshOutlineData {
+                f32v3 start, end;
+            };
+
+            struct NavmeshOutlines {
+                std::vector<NavmeshOutlineData> outlines;
+                std::atomic<bool>               is_dirty;
+                std::atomic<bool>               is_dead;
+                bool                            is_updated;
+                size_t                          last_size;
+                hg::MeshHandles                 mesh_handles;
+                hmem::WeakHandle<Chunk>         chunk;
+
+                NavmeshOutlines() :
+                    outlines{},
+                    is_dirty{},
+                    is_dead{},
+                    is_updated(false),
+                    last_size(0),
+                    mesh_handles{},
+                    chunk{} {
+                    /* Empty. */
+                }
+
+                NavmeshOutlines(const NavmeshOutlines& rhs) {
+                    outlines = rhs.outlines;
+                    is_dirty.store(rhs.is_dirty.load());
+                    is_dead.store(rhs.is_dead.load());
+                    is_updated   = rhs.is_updated;
+                    last_size    = rhs.last_size;
+                    mesh_handles = rhs.mesh_handles;
+                    chunk        = rhs.chunk;
+                }
+            };
+
+            using NavmeshOutlinesPerChunk
+                = std::unordered_map<ChunkID, NavmeshOutlines>;
         public:
             NavmeshOutlineRenderer();
 
@@ -29,30 +67,20 @@ namespace hemlock {
              */
             void dispose();
 
-            void update(FrameTime) { /* Empty. */
-            }
+            void update(FrameTime);
 
-            void draw(FrameTime time);
+            void draw(FrameTime);
 
             void register_chunk(hmem::Handle<Chunk> chunk);
         protected:
+            void __calculate_outlines(NavmeshOutlines& navmesh);
+
             Delegate<void(Sender)> handle_chunk_navmesh_change;
             Delegate<void(Sender)> handle_chunk_unload;
 
-            hg::MeshHandles m_navmesh_mesh_handles;
+            GLuint m_navmesh_outline_vao;
 
-            struct NavmeshOutlineData {
-                f32v3 start, end;
-            };
-
-            using NavmeshOutlines    = std::vector<NavmeshOutlineData>;
-            using NavmeshOutlineRefs = std::unordered_map<size_t, size_t>;
-
-            NavmeshOutlines    m_block_outlines;
-            NavmeshOutlineRefs m_block_outline_refs;
-            size_t             m_next_outline_id;
-
-            size_t m_last_outline_count;
+            NavmeshOutlinesPerChunk m_navmesh_outlines;
         };
     }  // namespace voxel
 }  // namespace hemlock
--- src/voxel/graphics/outline_renderer/navmesh.cpp
+++ src/voxel/graphics/outline_renderer/navmesh.cpp
@@ -1,119 +1,142 @@
 #include "stdafx.h"
 
+#include "voxel/chunk.h"
 #include "voxel/graphics/outline_renderer/navmesh.h"
 
 #include "maths/powers.hpp"
 
 hvox::NavmeshOutlineRenderer::NavmeshOutlineRenderer() :
-    handle_chunk_navmesh_change(Delegate<void(Sender)>{ [&](Sender) {
+    handle_chunk_navmesh_change(Delegate<void(Sender)>{ [&](Sender sender) {
+        auto chunk = sender.get_handle<Chunk>().lock();
 
+        m_navmesh_outlines[chunk->position.id].is_dirty.store(true);
     } }),
-    handle_chunk_unload(Delegate<void(Sender)>{ [&](Sender) {
+    handle_chunk_unload(Delegate<void(Sender)>{ [&](Sender sender) {
+        auto chunk = sender.get_handle<Chunk>().lock();
 
+        m_navmesh_outlines[chunk->position.id].is_dead.store(true);
     } }),
-    m_navmesh_mesh_handles{},
-    m_next_outline_id(1),
-    m_last_outline_count(0) {
+    m_navmesh_outline_vao(0) {
     // Empty.
 }
 
 void hvox::NavmeshOutlineRenderer::init() {
+    hg::MeshHandles tmp = { .vao = 0, .vbo = static_cast<GLuint>(-1) };
     hg::upload_mesh(
-        hg::Point_2D_32_MeshData{ nullptr, 0 },
-        m_navmesh_mesh_handles,
-        hg::MeshDataVolatility::STATIC
+        hg::Point_2D_32_MeshData{ nullptr, 0 }, tmp, hg::MeshDataVolatility::STATIC
     );
+    m_navmesh_outline_vao = tmp.vao;
 }
 
 void hvox::NavmeshOutlineRenderer::dispose() {
-    BlockOutlines().swap(m_block_outlines);
-    BlockOutlineRefs().swap(m_block_outline_refs);
+    for (auto& [chunk_id, navmesh] : m_navmesh_outlines) {
+        hg::dispose_mesh(hg::MeshHandles{ 0, navmesh.mesh_handles.vbo });
+    }
+    hg::dispose_mesh(hg::MeshHandles{ m_navmesh_outline_vao, 0 });
 
-    ui32 prev_ref_count = ref_count.fetch_sub(1);
-    if (prev_ref_count == 1) {
-        hg::dispose_mesh(m_navmesh_mesh_handles);
+    m_navmesh_outline_vao = 0;
 
-        m_navmesh_mesh_handles = {};
+    NavmeshOutlinesPerChunk().swap(m_navmesh_outlines);
+}
+
+void hvox::NavmeshOutlineRenderer::update(FrameTime) {
+    for (auto& [chunk_id, navmesh] : m_navmesh_outlines) {
+        if (navmesh.is_dirty.load()) {
+            __calculate_outlines(navmesh);
+
+            navmesh.is_updated = true;
+        }
     }
 }
 
 void hvox::NavmeshOutlineRenderer::draw(FrameTime) {
-    size_t last_size = hmaths::next_power_2(m_last_outline_count);
-    size_t this_size = hmaths::next_power_2(m_block_outlines.size());
+    for (auto& [chunk_id, navmesh] : m_navmesh_outlines) {
+        if (navmesh.is_updated && !navmesh.is_dead.load()) {
+            size_t last_size = hmaths::next_power_2(navmesh.last_size);
+            size_t this_size = hmaths::next_power_2(navmesh.outlines.size());
 
-    // If VBO buffer needs to be increased to fit in all outlines, then do so.
-    // TODO(Matthew): shrink this also?
-    if (last_size < this_size) {
-        glNamedBufferData(
-            m_instance_vbo, this_size * sizeof(OutlineData), nullptr, GL_DYNAMIC_DRAW
-        );
-    }
+            if (last_size < this_size) {
+                glNamedBufferData(
+                    navmesh.mesh_handles.vbo,
+                    this_size * sizeof(NavmeshOutlineData),
+                    nullptr,
+                    GL_DYNAMIC_DRAW
+                );
 
-    // TODO(Matthew): We are sending this data every frame?! Might be fine
-    //                for debug purposes, but we gotta be sure this is
-    //                all people want to use this for.
-    glNamedBufferSubData(
-        m_instance_vbo,
-        0,
-        m_block_outlines.size() * sizeof(OutlineData),
-        reinterpret_cast<void*>(m_block_outlines.data())
-    );
+                glNamedBufferSubData(
+                    navmesh.mesh_handles.vbo,
+                    0,
+                    navmesh.outlines.size() * sizeof(NavmeshOutlineData),
+                    reinterpret_cast<void*>(navmesh.outlines.data())
+                );
 
-    glBindVertexArray(m_navmesh_mesh_handles.vao);
+                navmesh.last_size = this_size;
+            }
 
-    glVertexArrayVertexBuffer(
-        m_navmesh_mesh_handles.vao, 1, m_instance_vbo, 0, sizeof(OutlineData)
-    );
+            navmesh.is_updated = false;
+        }
 
-    glDrawArraysInstanced(
-        GL_LINES, 0, BLOCK_OUTLINE_VERTEX_COUNT, m_block_outlines.size()
-    );
-}
+        glBindVertexArray(navmesh.mesh_handles.vao);
 
-size_t hvox::NavmeshOutlineRenderer::add_outline(OutlineData&& outline) {
-    auto [_, success]
-        = m_block_outline_refs.try_emplace(m_next_outline_id, m_block_outlines.size());
-
-    if (!success) return 0;
-
-    m_block_outlines.emplace_back(std::forward<OutlineData>(outline));
-
-    return m_next_outline_id++;
-}
-
-bool hvox::NavmeshOutlineRenderer::modify_outline(
-    size_t outline_id, OutlineData&& outline
-) {
-    try {
-        size_t idx = m_block_outline_refs.at(outline_id);
-
-        m_block_outlines[idx] = std::forward<OutlineData>(outline);
-    } catch (std::out_of_range& e) {
-        return false;
-    }
-
-    return true;
-}
-
-bool hvox::NavmeshOutlineRenderer::remove_outline(size_t outline_id) {
-    auto it = m_block_outline_refs.find(outline_id);
-    if (it != m_block_outline_refs.end()) {
-        m_block_outlines[it->second] = std::move(m_block_outlines.back());
-
-        auto back_it = std::find_if(
-            m_block_outline_refs.begin(),
-            m_block_outline_refs.end(),
-            [&](auto el) { return el.second == m_block_outlines.size() - 1; }
+        glVertexArrayVertexBuffer(
+            navmesh.mesh_handles.vao,
+            0,
+            navmesh.mesh_handles.vbo,
+            0,
+            sizeof(OutlineData)
         );
 
-        back_it->second = it->second;
+        glDrawArrays(GL_LINES, 0, navmesh.outlines.size());
+    }
+}
 
-        m_block_outlines.pop_back();
+void hvox::NavmeshOutlineRenderer::register_chunk(hmem::Handle<Chunk> chunk) {
+    NavmeshOutlines tmp = {};
+    tmp.chunk           = chunk;
+    auto [_, success]   = m_navmesh_outlines.try_emplace(chunk->position.id, tmp);
 
-        m_block_outline_refs.erase(it);
-    } else {
-        return false;
+    if (success) {
+        chunk->on_navmesh_change += &handle_chunk_navmesh_change;
+        chunk->on_unload         += &handle_chunk_unload;
+    }
+}
+
+void hvox::NavmeshOutlineRenderer::__calculate_outlines(NavmeshOutlines& navmesh) {
+    hmem::Handle<Chunk> chunk = navmesh.chunk.lock();
+
+    if (chunk == nullptr) {
+        navmesh.is_dead.store(true);
+        return;
     }
 
-    return true;
+    auto lock = std::shared_lock(chunk->navmesh_mutex);
+
+    std::vector<NavmeshOutlineData> tmp_outline_buffer;
+    tmp_outline_buffer.reserve(navmesh.outlines.size());
+
+    for (auto vertex :
+         boost::make_iterator_range(boost::vertices(chunk->navmesh.graph)))
+    {
+        ai::ChunkNavmeshNode node = chunk->navmesh.vertex_coord_map[vertex];
+        BlockWorldPosition   vert_pos
+            = block_world_position(node.chunk_pos, node.block_pos);
+
+        for (auto edge :
+             boost::make_iterator_range(boost::out_edges(vertex, chunk->navmesh.graph)))
+        {
+            auto target_vertex = boost::target(edge, chunk->navmesh.graph);
+
+            ai::ChunkNavmeshNode target_node
+                = chunk->navmesh.vertex_coord_map[target_vertex];
+            BlockWorldPosition target_vert_pos
+                = block_world_position(target_node.chunk_pos, target_node.block_pos);
+
+            tmp_outline_buffer.emplace_back(
+                f32v3{ static_cast<f32v3>(vert_pos) },
+                f32v3{ static_cast<f32v3>(target_vert_pos) }
+            );
+        }
+    }
+
+    navmesh.outlines.swap(tmp_outline_buffer);
 }
