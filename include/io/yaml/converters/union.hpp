bc088e02729783b6802f38c7f115b3b3893fafc8
Matthew Marshall <matthew.marshall090396@gmail.com>
2023-07-05 18:42:33 +0100

    Rewrite union YAML macros to correctly set up the tag enum, union, and converters.

--- include/io/yaml/converters/union.hpp
+++ include/io/yaml/converters/union.hpp
@@ -4,6 +4,14 @@
 // TODO(Matthew): Fix defining enum we want serialisable without using enum
 //                serialisation macros.
 
+#if !defined(H_POD_STRUCT)
+#  define H_POD_STRUCT 1
+#endif  // !defined(H_POD_STRUCT)
+
+#if !defined(H_NON_POD_TYPE)
+#  define H_NON_POD_TYPE 0
+#endif  // !defined(H_NON_POD_TYPE)
+
 #if !defined(H_FIELD_NAME)
 #  define H_FIELD_NAME(NAME, TYPE) NAME
 #endif  //! defined(H_FIELD_NAME)
@@ -14,23 +22,16 @@
 #  define H_FIELD_TYPE(NAME, TYPE) TYPE
 #endif  //! defined(H_FIELD_TYPE)
 
-#if !defined(H_UNION_ELEM_VALUE)
-#  define H_UNION_ELEM_VALUE(_, VALUE, TYPE) VALUE
-#endif  //! defined(H_UNION_ELEM_VALUE)
-#if !defined(H_UNION_ELEM_TYPE)
-#  define H_UNION_ELEM_TYPE(_, VALUE, TYPE) TYPE
-#endif  //! defined(H_UNION_ELEM_TYPE)
+#if !defined(H_UNION_ENTRY_NAME)
+#  define H_UNION_ENTRY_NAME(IS_POD_STRUCT, NAME, ...) NAME
+#endif  //! defined(H_UNION_ENTRY_NAME)
 
-#define H_UNION_ENTRY()         1
-#define H_NEXT_UNION_KIND()     1
-#define H_CONTINUE_UNION_KIND() 0
+#if !defined(H_INDIRECT_UNION_ENTRY_NAME)
+#  define H_INDIRECT_UNION_ENTRY_NAME(ARGS) H_UNION_ENTRY_NAME ARGS
+#endif  //! defined(H_INDIRECT_UNION_ENTRY_NAME)
 
 #if !defined(H_DEF_YAML_CONVERSION_OF_UNION)
-#  define H_SWITCH_UNION_CASE(KIND_TYPE, KIND)                                         \
-    break;                                                                             \
-    }                                                                                  \
-    case KIND_TYPE ::KIND:                                                             \
-    {
+
 #  if !defined(H_WRITE_UNION_FIELD_ENCODE)
 #    define H_WRITE_UNION_FIELD_ENCODE(FIELD_DATA)                                     \
       node[H_FIELD_NAME_STR FIELD_DATA] = YAML::Node(val.H_FIELD_NAME FIELD_DATA);
@@ -46,14 +47,24 @@
       }
 #  endif  //! defined(H_WRITE_UNION_FIELD_DECODE)
 
-#  define H_WRITE_UNION_ENCODE(KIND_TYPE, ELEM)                                        \
-    IF_ELSE(BOOL(H_UNION_ELEM_TYPE ELEM))                                              \
-    (DEFER4(H_SWITCH_UNION_CASE)(KIND_TYPE, H_UNION_ELEM_VALUE ELEM),                  \
-     DEFER4(H_WRITE_UNION_FIELD_ENCODE)(H_UNION_ELEM_VALUE ELEM))
-#  define H_WRITE_UNION_DECODE(KIND_TYPE, ELEM)                                        \
-    IF_ELSE(BOOL(H_UNION_ELEM_TYPE ELEM))                                              \
-    (DEFER4(H_SWITCH_UNION_CASE)(KIND_TYPE, H_UNION_ELEM_VALUE ELEM),                  \
-     DEFER4(H_WRITE_UNION_FIELD_DECODE)(H_UNION_ELEM_VALUE ELEM))
+#  define H_WRITE_UNION_ENCODE(IS_POD_STRUCT, NAME, ...)                               \
+NAME:                                                                                  \
+{                                                                                      \
+MAP(H_WRITE_UNION_FIELD_ENCODE, EMPTY, __VA_ARGS__)                                    \
+break;                                                                                 \
+}
+
+#  define H_WRITE_UNION_DECODE(IS_POD_STRUCT, NAME, ...)                               \
+NAME:                                                                                  \
+{                                                                                      \
+MAP(H_WRITE_UNION_FIELD_DECODE, EMPTY, __VA_ARGS__)                                    \
+break;                                                                                 \
+}
+
+#  define H_INDIRECT_WRITE_UNION_ENCODE(KIND_TYPE, ARGS)                               \
+    case KIND_TYPE ::H_WRITE_UNION_ENCODE ARGS
+#  define H_INDIRECT_WRITE_UNION_DECODE(KIND_TYPE, ARGS)                               \
+    case KIND_TYPE ::H_WRITE_UNION_DECODE ARGS
 
 #  define H_DEF_YAML_CONVERSION_OF_UNION(UNION_TYPE, KIND_UNDERLYING_TYPE, ...)        \
     namespace YAML {                                                                   \
@@ -72,10 +83,13 @@
                   static_cast<KIND_UNDERLYING_TYPE>(val.kind)                          \
               );                                                                       \
               return YAML::Node();                                                     \
-              BIND_MAP(                                                                \
-                  H_WRITE_UNION_ENCODE, CAT(UNION_TYPE, Kind), EMPTY, __VA_ARGS__      \
-              )                                                                        \
             }                                                                          \
+              BIND_MAP_2(                                                              \
+                  H_INDIRECT_WRITE_UNION_ENCODE,                                       \
+                  CAT(UNION_TYPE, Kind),                                               \
+                  EMPTY,                                                               \
+                  __VA_ARGS__                                                          \
+              )                                                                        \
           }                                                                            \
                                                                                        \
           return node;                                                                 \
@@ -91,13 +105,16 @@
             {                                                                          \
               debug_printf(                                                            \
                   "Could not encode MyUnion with kind %i",                             \
-                  static_cast<KIND_UNDERLYING_TYPE>(val.kind)                          \
+                  static_cast<KIND_UNDERLYING_TYPE>(result.kind)                       \
               );                                                                       \
               return false;                                                            \
-              BIND_MAP(                                                                \
-                  H_WRITE_UNION_DECODE, CAT(UNION_TYPE, Kind), EMPTY, __VA_ARGS__      \
-              )                                                                        \
             }                                                                          \
+              BIND_MAP_2(                                                              \
+                  H_INDIRECT_WRITE_UNION_DECODE,                                       \
+                  CAT(UNION_TYPE, Kind),                                               \
+                  EMPTY,                                                               \
+                  __VA_ARGS__                                                          \
+              )                                                                        \
           }                                                                            \
                                                                                        \
           return true;                                                                 \
@@ -107,20 +124,23 @@
 #endif  //! defined(H_DEF_YAML_CONVERSION_OF_UNION)
 
 #if !defined(H_DEF_UNION_WITH_YAML_CONVERSION)
-#  define H_SWITCH_UNION_STRUCT()                                                      \
-    }                                                                                  \
-    ;                                                                                  \
-    struct {
-#  define H_WRITE_UNION_FIELD(FIELD_DATA)                                              \
+#  define H_UNION_ENTRY_TO_FIELD(FIELD_DATA)                                           \
     H_FIELD_TYPE FIELD_DATA H_FIELD_NAME FIELD_DATA
+#  define H_WRITE_UNION_KIND(ELEM) H_UNION_ENTRY_NAME ELEM,
 
-#  define H_WRITE_UNION_KIND(ELEM)                                                     \
-    IF(BOOL(H_UNION_ELEM_TYPE ELEM)) (H_UNION_ELEM_VALUE ELEM, )
+#  define H_WRITE_UNION_POD_STRUCT_ENTRY(...)                                          \
+    struct {                                                                           \
+      MAP(H_UNION_ENTRY_TO_FIELD, SEMICOLON, __VA_ARGS__);                             \
+    };
+#  define H_WRITE_UNION_NON_POD_TYPE_ENTRY(FIELD_DATA)                                 \
+    H_UNION_ENTRY_TO_FIELD(FIELD_DATA);
 
-#  define H_WRITE_UNION_STRUCT_ELEMENT(ELEM)                                           \
-    IF_ELSE(BOOL(H_UNION_ELEM_TYPE ELEM))                                              \
-    (DEFER4(H_SWITCH_UNION_STRUCT)(),                                                  \
-     DEFER4(H_WRITE_UNION_FIELD)(H_UNION_ELEM_VALUE ELEM);)
+#  define H_WRITE_UNION_ENTRY(IS_POD_STRUCT, NAME, ...)                                \
+    IF_ELSE(BOOL(IS_POD_STRUCT))                                                       \
+    (H_WRITE_UNION_POD_STRUCT_ENTRY(__VA_ARGS__),                                      \
+     H_WRITE_UNION_NON_POD_TYPE_ENTRY(__VA_ARGS__))
+
+#  define H_INDIRECT_WRITE_UNION_ENTRY(ARGS) H_WRITE_UNION_ENTRY ARGS
 
 #  define H_DEF_UNION_WITH_YAML_CONVERSION(NAMESPACE, NAME, KIND_TYPE, ...)            \
     namespace NAMESPACE {                                                              \
@@ -129,12 +149,14 @@
       struct NAME {                                                                    \
         NAME##Kind kind;                                                               \
         union {                                                                        \
-          struct {                                                                     \
-            MAP(H_WRITE_UNION_STRUCT_ELEMENT, EMPTY, __VA_ARGS__)                      \
-          };                                                                           \
+          MAP_2(H_INDIRECT_WRITE_UNION_ENTRY, EMPTY, __VA_ARGS__)                      \
         };                                                                             \
       };                                                                               \
     }                                                                                  \
+    H_DECL_SERIALISATION_OF_ENUM(                                                      \
+        NAMESPACE, NAME##Kind, MAP(H_INDIRECT_UNION_ENTRY_NAME, COMMA, __VA_ARGS__)    \
+    )                                                                                  \
+    H_DEF_SERIALISATION_OF_ENUM(NAMESPACE, NAME##Kind)                                 \
     H_DEF_YAML_CONVERSION_OF_UNION(NAMESPACE ::NAME, KIND_TYPE, __VA_ARGS__)
 #endif  //! defined(H_DEF_UNION_WITH_YAML_CONVERSION)
 
