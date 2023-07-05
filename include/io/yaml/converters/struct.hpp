3438e8631ebbf0c02fde16b6e0d09ddc96c0ae21
Matthew Marshall <matthew.marshall090396@gmail.com>
2023-07-05 20:57:54 +0100

    Bring struct handling up to parity with union handling in serialisation.

--- include/io/yaml/converters/struct.hpp
+++ include/io/yaml/converters/struct.hpp
@@ -1,68 +1,67 @@
 #ifndef __hemlock_io_yaml_converters_struct_hpp
 #define __hemlock_io_yaml_converters_struct_hpp
 
+/*************************************\
+ * Generic helpers for field access. *
+\*************************************/
+
+#if !defined(H_FIELD_NAME)
+#  define H_FIELD_NAME(NAME, TYPE) NAME
+#endif  //! defined(H_FIELD_NAME)
+#if !defined(H_FIELD_NAME_STR)
+#  define H_FIELD_NAME_STR(NAME, TYPE) #NAME
+#endif  //! defined(H_FIELD_NAME_STR)
+#if !defined(H_FIELD_TYPE)
+#  define H_FIELD_TYPE(NAME, TYPE) TYPE
+#endif  //! defined(H_FIELD_TYPE)
+
+#if !defined(H_DECL_YAML_CONVERSION_OF_STRUCT)
+#  define H_DECL_YAML_CONVERSION_OF_STRUCT(NAMESPACE, NAME)                            \
+    namespace YAML {                                                                   \
+      template <>                                                                      \
+      struct convert<NAMESPACE::NAME> {                                                \
+        static Node encode(const NAMESPACE::NAME& val);                                \
+                                                                                       \
+        static bool decode(const Node& node, NAMESPACE::NAME& result);                 \
+      };                                                                               \
+    }
+#endif  //! defined(H_DECL_YAML_CONVERSION_OF_STRUCT)
+
 #if !defined(H_DEF_YAML_CONVERSION_OF_STRUCT)
-#  define H_STRUCT_ENTRY_NAME(NAME, TYPE) #NAME
-#  define H_STRUCT_ENTRY_FIELD(NAME, TYPE) NAME
-#  define H_STRUCT_ENTRY_TYPE(NAME, TYPE) TYPE
 
-#  if !defined(H_STRUCT_ENTRY_ENCODE)
-#    define H_STRUCT_ENTRY_ENCODE(ENTRY_INFO)           \
-node[H_STRUCT_ENTRY_NAME ENTRY_INFO] = YAML::Node(      \
-    val.H_STRUCT_ENTRY_FIELD ENTRY_INFO                 \
-)
-#  endif //!defined(H_STRUCT_ENTRY_ENCODE)
+#  if !defined(H_WRITE_STRUCT_ENTRY_ENCODE)
+#    define H_WRITE_STRUCT_ENTRY_ENCODE(FIELD_DATA)                                    \
+      node[H_FIELD_NAME_STR FIELD_DATA] = YAML::Node(val.H_FIELD_NAME FIELD_DATA)
+#  endif  //! defined(H_WRITE_STRUCT_ENTRY_ENCODE)
 
-#  if !defined(H_STRUCT_ENTRY_DECODE)
-#    define H_STRUCT_ENTRY_DECODE(ENTRY_INFO)           \
-if (                                                    \
-    !convert<H_STRUCT_ENTRY_TYPE ENTRY_INFO>::decode(   \
-        node[H_STRUCT_ENTRY_NAME ENTRY_INFO],           \
-        result.H_STRUCT_ENTRY_FIELD ENTRY_INFO          \
-    )                                                   \
-) {                                                     \
-    return false;                                       \
-}
-#  endif //!defined(H_STRUCT_ENTRY_DECODE)
+#  if !defined(H_WRITE_STRUCT_ENTRY_DECODE)
+#    define H_WRITE_STRUCT_ENTRY_DECODE(FIELD_DATA)                                    \
+      if (!convert<H_FIELD_TYPE FIELD_DATA>::decode(                                   \
+              node[H_FIELD_NAME_STR FIELD_DATA], result.H_FIELD_NAME FIELD_DATA        \
+          ))                                                                           \
+      {                                                                                \
+        return false;                                                                  \
+      }
+#  endif  //! defined(H_WRITE_STRUCT_ENTRY_DECODE)
 
-#  define H_DEF_YAML_CONVERSION_OF_STRUCT(TYPE, ...)                \
-namespace YAML {                                                    \
-    template <>                                                     \
-    struct convert<TYPE> {                                          \
-        static Node encode(const TYPE& val) {                       \
-            YAML::Node node(YAML::NodeType::Map);                   \
-                                                                    \
-            MAP(H_STRUCT_ENTRY_ENCODE, SEMICOLON, __VA_ARGS__);     \
-                                                                    \
-            return node;                                            \
-        }                                                           \
-                                                                    \
-        static bool decode(const Node& node, TYPE& result) {        \
-            MAP(H_STRUCT_ENTRY_DECODE, SEMICOLON, __VA_ARGS__);     \
-                                                                    \
-            return true;                                            \
-        }                                                           \
-    };                                                              \
-}
-#  endif //!defined(H_DEF_YAML_CONVERSION_OF_STRUCT)
+#  define H_DEF_YAML_CONVERSION_OF_STRUCT(NAMESPACE, NAME, ...)                        \
+    [[maybe_unused]] YAML::Node YAML::convert<NAMESPACE::NAME>::encode(                \
+        const NAMESPACE::NAME& val                                                     \
+    ) {                                                                                \
+      YAML::Node node(YAML::NodeType::Map);                                            \
+                                                                                       \
+      MAP(H_WRITE_STRUCT_ENTRY_ENCODE, SEMICOLON, __VA_ARGS__);                        \
+                                                                                       \
+      return node;                                                                     \
+    }                                                                                  \
+                                                                                       \
+    [[maybe_unused]] bool YAML::convert<NAMESPACE::NAME>::decode(                      \
+        const Node& node, NAMESPACE::NAME& result                                      \
+    ) {                                                                                \
+      MAP(H_WRITE_STRUCT_ENTRY_DECODE, SEMICOLON, __VA_ARGS__);                        \
+                                                                                       \
+      return true;                                                                     \
+    }
+#endif  //! defined(H_DEF_YAML_CONVERSION_OF_STRUCT)
 
-#if !defined(H_DEF_STRUCT_WITH_YAML_CONVERSION)
-#  if !defined(H_STRUCT_ENTRY_FIELD)
-#    define H_STRUCT_ENTRY_FIELD(NAME, TYPE) NAME
-#  endif //!defined(H_STRUCT_ENTRY_FIELD)
-#  if !defined(H_STRUCT_ENTRY_TYPE)
-#    define H_STRUCT_ENTRY_TYPE(NAME, TYPE) TYPE
-#  endif //!defined(H_STRUCT_ENTRY_TYPE)
-
-#  define H_STRUCT_ENTRY_TO_FIELD(ENTRY) H_STRUCT_ENTRY_TYPE ENTRY H_STRUCT_ENTRY_FIELD ENTRY
-
-#  define H_DEF_STRUCT_WITH_YAML_CONVERSION(NAMESPACE, NAME, ...)   \
-namespace NAMESPACE {                                               \
-    struct NAME {                                                   \
-        MAP(H_STRUCT_ENTRY_TO_FIELD, SEMICOLON, __VA_ARGS__);       \
-    };                                                              \
-}                                                                   \
-H_DEF_YAML_CONVERSION_OF_STRUCT(NAMESPACE :: NAME, __VA_ARGS__)
-#endif //!defined(H_DEF_STRUCT_WITH_YAML_CONVERSION)
-
-#endif // __hemlock_io_yaml_converters_struct_hpp
+#endif  // __hemlock_io_yaml_converters_struct_hpp
