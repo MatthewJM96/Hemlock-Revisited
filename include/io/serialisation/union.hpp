3a82ad2d0ecc7476c1e0e8ed3d43c3de4d8089e0
Matthew Marshall <matthew.marshall090396@gmail.com>
2023-07-05 18:41:35 +0100

    Include YAML converters broadly, as a union may rely on some other converter to be converted.

--- include/io/serialisation/union.hpp
+++ include/io/serialisation/union.hpp
@@ -1,15 +1,15 @@
 #ifndef __hemlock_io_serialisation_union_hpp
 #define __hemlock_io_serialisation_union_hpp
 
-#include "io/yaml/converters/union.hpp"
+#include "io/yaml.hpp"
 
 #if !defined(H_DECL_UNION_WITH_SERIALISATION)
 #  define H_DECL_UNION_WITH_SERIALISATION(NAMESPACE, NAME)
-#endif // !defined(H_DECL_UNION_WITH_SERIALISATION)
+#endif  // !defined(H_DECL_UNION_WITH_SERIALISATION)
 
 #if !defined(H_DEF_UNION_WITH_SERIALISATION)
-#  define H_DEF_UNION_WITH_SERIALISATION(NAMESPACE, NAME, KIND_TYPE, ...) \
+#  define H_DEF_UNION_WITH_SERIALISATION(NAMESPACE, NAME, KIND_TYPE, ...)              \
     H_DEF_UNION_WITH_YAML_CONVERSION(NAMESPACE, NAME, KIND_TYPE, __VA_ARGS__)
-#endif // !defined(H_DEF_UNION_WITH_SERIALISATION)
+#endif  // !defined(H_DEF_UNION_WITH_SERIALISATION)
 
-#endif // __hemlock_io_serialisation_union_hpp
+#endif  // __hemlock_io_serialisation_union_hpp
