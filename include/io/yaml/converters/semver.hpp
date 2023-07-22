11424e3b8d880ab643db6e35d854ec5697a3a9e6
Matthew Marshall <matthew.marshall090396@gmail.com>
2023-07-22 16:47:49 +0100

    Imlpement YAML conversion of SemanticVersion objects.

---
+++ include/io/yaml/converters/semver.hpp
@@ -0,0 +1,23 @@
+#ifndef __hemlock_io_yaml_converters_semver_hpp
+#define __hemlock_io_yaml_converters_semver_hpp
+
+#include "semver.h"
+
+namespace YAML {
+    template <>
+    struct convert<hemlock::SemanticVersion> {
+        static Node encode(const hemlock::SemanticVersion& semver) {
+            return Node{ semver.string() };
+        }
+
+        static bool decode(const Node& node, hemlock::SemanticVersion& semver) {
+            if (!node.IsScalar()) {
+                return false;
+            }
+
+            return semver.load(node.as<std::string>());
+        }
+    };
+}  // namespace YAML
+
+#endif  // __hemlock_io_yaml_converters_semver_hpp
