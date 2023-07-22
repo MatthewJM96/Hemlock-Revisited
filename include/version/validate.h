18872f990f4ef5f29ed9baf874cd897a4dfc86cd
Matthew Marshall <matthew.marshall090396@gmail.com>
2023-07-22 19:45:42 +0100

    Add a union of ways to encode a set of versions.

--- include/version/validate.h
+++ include/version/validate.h
@@ -12,4 +12,14 @@ H_DECL_STRUCT_WITH_SERIALISATION(
     (maximum, hemlock::SemanticVersion)
 )
 
+H_DECL_UNION_WITH_SERIALISATION(
+    hemlock::mod,
+    Versions,
+    ui8,
+    (H_NON_POD_TYPE(), LIST, (list, hemlock::VersionList)),
+    (H_NON_POD_TYPE(), RANGE, (range, hemlock::VersionRange)),
+    (H_NON_POD_TYPE(), MINIMUM, (minimum, hemlock::SemanticVersion)),
+    (H_NON_POD_TYPE(), MAXIMUM, (maximum, hemlock::SemanticVersion)),
+)
+
 #endif  // __hemlock_version_validate_h
--- src/version/validate.cpp
+++ src/version/validate.cpp
@@ -12,3 +12,13 @@ H_DEF_STRUCT_WITH_SERIALISATION(
     (minimum, hemlock::SemanticVersion),
     (maximum, hemlock::SemanticVersion)
 )
+
+H_DEF_UNION_WITH_SERIALISATION(
+    hemlock::mod,
+    Versions,
+    ui8,
+    (H_NON_POD_TYPE(), LIST, (list, hemlock::VersionList)),
+    (H_NON_POD_TYPE(), RANGE, (range, hemlock::VersionRange)),
+    (H_NON_POD_TYPE(), MINIMUM, (minimum, hemlock::SemanticVersion)),
+    (H_NON_POD_TYPE(), MAXIMUM, (maximum, hemlock::SemanticVersion)),
+)
