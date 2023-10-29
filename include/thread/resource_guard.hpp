bf981019f8b130d6ce23442528aa0575b4701e60
Matthew Marshall <matthew.marshall090396@gmail.com>
2023-10-29 14:48:13 +0000

    Move logic of resource guard out of instance manager for reuse.

---
+++ include/thread/resource_guard.hpp
@@ -0,0 +1,40 @@
+#ifndef __hemlock_thread_resource_guard_hpp
+#define __hemlock_thread_resource_guard_hpp
+
+namespace hemlock {
+    namespace thread {
+        template <typename ResourceType>
+        class ResourceGuard {
+        public:
+            ResourceGuard() {
+                // Empty.
+            }
+
+            ~ResourceGuard() {
+                // Empty.
+            }
+
+            void init(ResourceType&& resource) {
+                std::unique_lock lock(m_mutex);
+
+                m_resource = std::forward<ResourceType>(resource);
+            }
+
+            ResourceType& get(std::unique_lock<std::shared_mutex>& lock) {
+                lock = std::unique_lock(m_mutex);
+                return m_resource;
+            }
+
+            const ResourceType& get(std::shared_lock<std::shared_mutex>& lock) {
+                lock = std::shared_lock(m_mutex);
+                return m_resource;
+            }
+        protected:
+            std::shared_mutex m_mutex;
+            ResourceType      m_resource;
+        };
+    }  // namespace thread
+}  // namespace hemlock
+namespace hthread = hemlock::thread;
+
+#endif  // __hemlock_thread_resource_guard_hpp
--- src/voxel/graphics/mesh/instance_manager.cpp
+++ src/voxel/graphics/mesh/instance_manager.cpp
@@ -1,8 +1,8 @@
 #include "voxel/graphics/mesh/instance_manager.h"
 #include "stdafx.h"
 
-hvox::ChunkInstanceManager::ChunkInstanceManager() :
-    m_instance({ nullptr, 0 }) { /* Empty. */
+hvox::ChunkInstanceManager::ChunkInstanceManager() {
+    m_resource = { nullptr, 0 };
 }
 
 void hvox::ChunkInstanceManager::init(hmem::Handle<ChunkInstanceDataPager> data_pager) {
@@ -15,29 +15,17 @@ void hvox::ChunkInstanceManager::dispose() {
     m_data_pager = nullptr;
 }
 
-hvox::ChunkInstance&
-hvox::ChunkInstanceManager::get(std::unique_lock<std::shared_mutex>& lock) {
-    lock = std::unique_lock(m_mutex);
-    return m_instance;
-}
-
-const hvox::ChunkInstance&
-hvox::ChunkInstanceManager::get(std::shared_lock<std::shared_mutex>& lock) {
-    lock = std::shared_lock(m_mutex);
-    return m_instance;
-}
-
 void hvox::ChunkInstanceManager::generate_buffer() {
     std::unique_lock lock(m_mutex);
 
-    m_instance.count = 0;
-    if (!m_instance.data) m_instance.data = m_data_pager->get_page();
+    m_resource.count = 0;
+    if (!m_resource.data) m_resource.data = m_data_pager->get_page();
 }
 
 void hvox::ChunkInstanceManager::free_buffer() {
     std::unique_lock lock(m_mutex);
 
-    m_instance.count = 0;
-    if (m_instance.data) m_data_pager->free_page(m_instance.data);
-    m_instance.data = nullptr;
+    m_resource.count = 0;
+    if (m_resource.data) m_data_pager->free_page(m_resource.data);
+    m_resource.data = nullptr;
 }
