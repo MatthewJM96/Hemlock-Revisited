a5698751c022a7e59b84ae4cad220c30847e29c8
Matthew Marshall <matthew.marshall090396@gmail.com>
2023-11-01 18:52:46 +0000

    Decay resourcetype and make sure that for constness this is transferred to the contents pointed-to too.

--- include/thread/resource_guard.hpp
+++ include/thread/resource_guard.hpp
@@ -6,6 +6,12 @@ namespace hemlock {
         template <typename ResourceType>
         class ResourceGuard {
         public:
+            using resource_t       = std::decay_t<ResourceType>;
+            using const_resource_t = std::conditional_t<
+                std::is_pointer_v<std::decay_t<ResourceType>>,
+                const std::remove_pointer_t<std::decay_t<ResourceType>>* const,
+                const std::decay_t<ResourceType>>;
+
             ResourceGuard() : m_resource({}) {
                 // Empty.
             }
@@ -14,24 +20,24 @@ namespace hemlock {
                 // Empty.
             }
 
-            void init(ResourceType&& resource) {
+            void init(resource_t&& resource) {
                 std::unique_lock lock(m_mutex);
 
-                m_resource = std::forward<ResourceType>(resource);
+                m_resource = std::forward<resource_t>(resource);
             }
 
-            ResourceType& get(std::unique_lock<std::shared_mutex>& lock) {
+            resource_t& get(std::unique_lock<std::shared_mutex>& lock) {
                 lock = std::unique_lock(m_mutex);
                 return m_resource;
             }
 
-            const ResourceType& get(std::shared_lock<std::shared_mutex>& lock) {
+            const_resource_t& get(std::shared_lock<std::shared_mutex>& lock) {
                 lock = std::shared_lock(m_mutex);
                 return m_resource;
             }
         protected:
             std::shared_mutex m_mutex;
-            ResourceType      m_resource;
+            resource_t        m_resource;
         };
     }  // namespace thread
 }  // namespace hemlock
