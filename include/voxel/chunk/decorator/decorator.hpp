6718aed5766e89dd7a2f8f1409f510afd1f821ec
Matthew Marshall <matthew.marshall090396@gmail.com>
2023-11-04 15:27:32 +0000

    Add some concepts to begin defining expectations of decorators for composing chunks.

---
+++ include/voxel/chunk/decorator/decorator.hpp
@@ -0,0 +1,31 @@
+#ifndef __hemlock_voxel_chunk_decorator_decorator_hpp
+#define __hemlock_voxel_chunk_decorator_decorator_hpp
+
+namespace hemlock {
+    namespace voxel {
+        struct Chunk;
+
+        template <typename ChunkDecoratorCandidate>
+        concept ChunkDecorator = requires (ChunkDecoratorCandidate d) {
+                                     typename ChunkDecoratorCandidate::type;
+                                     {
+                                         d.attach_to_events()
+                                         } -> std::same_as<void>;
+                                 };
+
+        // TODO(Matthew): Arguments...
+        template <typename ChunkMeshDecoratorCandidate>
+        concept ChunkMeshDecorator = ChunkDecorator<ChunkMeshDecoratorCandidate>
+                                     && requires (ChunkMeshDecoratorCandidate d) {
+                                            {
+                                                d.add_mesh()
+                                                } -> std::same_as<void>;
+                                            {
+                                                d.remesh()
+                                                } -> std::same_as<void>;
+                                        };
+    }  // namespace voxel
+}  // namespace hemlock
+namespace hvox = hemlock::voxel;
+
+#endif  // __hemlock_voxel_chunk_decorator_decorator_hpp
