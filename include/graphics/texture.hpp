3d90c6ea5a9048aa88bb9becb48b6b66228a2af1
Matthew Marshall <matthew.marshall090396@gmail.com>
2023-11-26 09:53:09 +0000

    Add Mac support to texture.hpp.

--- include/graphics/texture.hpp
+++ include/graphics/texture.hpp
@@ -12,6 +12,7 @@ namespace hemlock {
             if (!hio::img::png::load(filepath, data, dims, format)) return 0;
 
             GLuint tex_id = 0;
+#if !defined(HEMLOCK_OS_MAC)
             glCreateTextures(GL_TEXTURE_2D, 1, &tex_id);
 
             GLsizei max_mipmaps
@@ -35,8 +36,6 @@ namespace hemlock {
 
             glGenerateTextureMipmap(tex_id);
 
-            delete[] data;
-
             // ui8* pixels = new ui8[dims.x * dims.y * 4];
 
             // glGetTextureImage(tex_id, 0, GL_RGBA, GL_UNSIGNED_BYTE, dims.x * dims.y
@@ -44,6 +43,69 @@ namespace hemlock {
 
             // hio::img::png::save(filepath + "_rendered.png",
             // reinterpret_cast<void*>(pixels), dims, format);
+#else   // !defined(HEMLOCK_OS_MAC)
+        // Create the texture we will put each glyph into.
+            glGenTextures(1, &tex_id);
+            glBindTexture(GL_TEXTURE_2D, tex_id);
+
+            GLsizei max_mipmaps
+                = 1
+                  + static_cast<GLsizei>(
+                      glm::log2(static_cast<f32>(glm::max(dims.x, dims.y)))
+                  );
+
+            // TODO(Matthew): use format to correctly store sub image.
+
+            GLsizei width  = dims.x;
+            GLsizei height = dims.y;
+            for (GLint level = 0; level < max_mipmaps; ++level) {
+                glTexImage2D(
+                    GL_TEXTURE_2D,
+                    max_mipmaps,
+                    GL_RGBA8,
+                    1,
+                    width,
+                    height,
+                    GL_RGBA,
+                    GL_UNSIGNED_BYTE,
+                    nullptr
+                );
+
+                if (width == 1 && height == 1) {
+                    max_mipmaps = level + 1;
+                }
+
+                width  = max(1, width / 2);
+                height = max(1, height / 2);
+            }
+
+            glTexSubImage2D(
+                GL_TEXTURE_2D, 0, 0, 0, dims.x, dims.y, GL_RGBA, GL_UNSIGNED_BYTE, data
+            );
+
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            glTexParameteri(
+                GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR
+            );
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_REPEAT);
+
+            glGenerateTexMipmap(GL_TEXTURE_2D);
+
+            // ui8* pixels = new ui8[dims.x * dims.y * 4];
+
+            // glGetTexImage(GL_TEXTURE_2D, 0, GL_RGBA, GL_UNSIGNED_BYTE, dims.x *
+            // dims.y
+            // * 4, pixels);
+
+            // hio::img::png::save(filepath + "_rendered.png",
+            // reinterpret_cast<void*>(pixels), dims, format);
+
+            glBindTexture(GL_TEXTURE_2D, 0);
+#endif  // !defined(HEMLOCK_OS_MAC)
+
+            delete[] data;
 
             return tex_id;
         }
