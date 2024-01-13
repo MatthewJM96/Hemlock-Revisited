612ae40faebc7387eaebdc359932c2cfaf4bb55d
Matthew Marshall <matthew.marshall090396@gmail.com>
2024-01-13 23:47:39 +0000

    Some work towards supporting different strategies for prioritisation.

--- include/thread/queue/priority_task_queue.hpp
+++ include/thread/queue/priority_task_queue.hpp
@@ -5,11 +5,35 @@
 
 namespace hemlock {
     namespace thread {
-        template <IsTaskQueue... Queues>
-        class PriorityTaskQueue {
+        using Priorities = std::map<size_t, size_t>;
+
+        /**
+         * @brief Defines a struct or class whose opeartor() evaluates which underlying
+         * queue to dequeue from. If that queue is empty, the next lower priority queue
+         * is pulled from and so on.
+         */
+        template <typename Candidate>
+        concept IsPriorityTaskQueueStrategy
+            = requires { typename Candidate::State; }
+              && requires (
+                  Candidate                  candidate,
+                  typename Candidate::State* state,
+                  const Priorities&          priorities
+              ) {
+                     {
+                         candidate.operator()(state, priorities)
+                         } -> std::same_as<Priorities::const_iterator>;
+                 };
+
+        template <IsPriorityTaskQueueStrategy Strategy, IsTaskQueue... Queues>
+        class GenericPriorityTaskQueue {
         public:
+            GenericPriorityTaskQueue() : m_queues{}, m_priorities{}, m_state{} {
+                // Empty.
+            }
+
             struct ControlBlock {
-                ControlBlock(PriorityTaskQueue* queue) {
+                ControlBlock(GenericPriorityTaskQueue* queue) {
                     for (size_t idx = 0; idx < sizeof...(Queues); ++idx) {
                         std::get<idx>(control_blocks) = { std::get<idx>(m_queues) };
                     }
@@ -43,19 +67,24 @@ namespace hemlock {
                 BasicTaskQueue** queue,
                 void*            control_block
             ) {
-                // TODO(Matthew): Implement a priority algorithm here.... actually do it
-                //                as a templated strategy!!!
-                (void)item;
-                (void)timeout;
-                (void)queue;
-                (void)control_block;
+                Priorities::const_iterator it = Strategy{}(&m_state, m_priorities);
+
+                // Note that to make this work we probably need to use actual
+                // inheritance across queues so that we can say "use this in-common
+                // function". At which point we probably don't even need to use tuple
+                // any more as opposed to an array of pointers to the base type.
+                invoke_on(m_queues, it->second, dequeue
 
                 return false;
             }
         protected:
-            std::tuple<Queues...>              m_queues;
-            std::unordered_map<size_t, size_t> m_priorities;
+            std::tuple<Queues...>    m_queues;
+            Priorities               m_priorities;
+            typename Strategy::State m_state;
         };
+
+        template <IsTaskQueue... Queues>
+        using PriorityTaskQueue = GenericPriorityTaskQueue<SomeStrategy, Queues...>;
     }  // namespace thread
 }  // namespace hemlock
 namespace hthread = hemlock::thread;
