6795c3733d85fd4a77594aa37d4b55ec568c96c3
Matthew Marshall <matthew.marshall090396@gmail.com>
2024-01-28 22:04:46 +0000

    First pass at a checker task queue.

---
+++ include/thread/queue/checker_task_queue.hpp
@@ -0,0 +1,94 @@
+#ifndef __hemlock_thread_queue_checker_task_queue_hpp
+#define __hemlock_thread_queue_checker_task_queue_hpp
+
+#include "thread/queue/state.h"
+
+namespace hemlock {
+    namespace thread {
+        template <size_t CheckerCount, IsBasicTaskQueue QueueType = BasicTaskQueue>
+        class CheckerTaskQueue {
+        public:
+            CheckerTaskQueue() : m_queue{} {
+                // Empty.
+            }
+
+            struct ControlBlock {
+                template <size_t... Indices>
+                void
+                init_control_blocks(CheckerTaskQueue* queue, std::index_sequence<Indices...>) {
+                    control_blocks = std::array<QueueType, CheckerCount>{
+                        typename QueueType::ControlBlock{ &queue->m_queues[Indices] }...
+                    };
+                }
+
+                ControlBlock(CheckerTaskQueue* queue) : control_blocks{} {
+                    init_control_blocks(
+                        queue, std::make_index_sequence<CheckerCount>()
+                    );
+                }
+
+                std::array<QueueType::ControlBlock, CheckerCount> control_blocks;
+            };
+
+            ControlBlock* register_thread() { return new ControlBlock{ this }; }
+
+            ControlBlock* register_threads(size_t count) {
+                // ProducerToken and ConsumerToken have no default constructor.
+
+                void*         mem            = new ui8[count * sizeof(ControlBlock)];
+                ControlBlock* control_blocks = reinterpret_cast<ControlBlock*>(mem);
+
+                for (size_t idx = 0; idx < count; ++idx) {
+                    control_blocks[idx] = ControlBlock{ this };
+                }
+
+                return control_blocks;
+            }
+
+            bool enqueue(size_t idx, const QueuedTask& item, void* control_block) {
+                auto& queue = m_queues[idx];
+                auto& underlying_control_block
+                    = reinterpret_cast<ControlBlock*>(control_block)
+                          ->control_blocks[idx];
+
+                return queue.enqueue(item, &underlying_control_block);
+            }
+
+            bool enqueue(size_t idx, QueuedTask&& item, void* control_block) {
+                auto& queue = m_queues[idx];
+                auto& underlying_control_block
+                    = reinterpret_cast<ControlBlock*>(control_block)
+                          ->control_blocks[idx];
+
+                return queue.enqueue(
+                    std::forward<QueuedTask>(item), underlying_control_block
+                );
+            }
+
+            bool dequeue(
+                OUT QueuedTask*      item,
+                TimingRep            timeout,
+                OUT BasicTaskQueue** queue,
+                void*                control_block
+            ) {
+                // TODO(Matthew): implement
+            }
+        protected:
+            std::array<QueueType, CheckerCount> m_queues;
+        };
+
+        template <size_t CheckerCount, IsBasicTaskQueue QueueType>
+        bool dequeue(
+            CheckerTaskQueue<CheckerCount, QueuesType>& queue,
+            OUT QueuedTask*                             item,
+            TimingRep                                   timeout,
+            OUT BasicTaskQueue**                        queue_out,
+            void*                                       control_block
+        ) {
+            return queue.dequeue(item, timeout, queue_out, control_block);
+        }
+    }  // namespace thread
+}  // namespace hemlock
+namespace hthread = hemlock::thread;
+
+#endif  // __hemlock_thread_queue_checker_task_queue_hpp
